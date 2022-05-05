8971d4d0d7a33d03bb57a3ac0ea90ee256a63d2e
Braden Wong <bmw02002turbo@gmail.com>
2022-05-05 02:09:43 -0400

    Add Open Drawer Button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,24 +11,39 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<div class="drawer">
+	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<!-- Page content here -->
+		<label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+		<section>
+			<h1>
+				<div class="welcome">
+					<picture>
+						<source srcset="svelte-welcome.webp" type="image/webp" />
+						<img src="svelte-welcome.png" alt="Welcome" />
+					</picture>
+				</div>
 
-		to your new<br />SvelteKit app
-	</h1>
+				to your new<br />SvelteKit app
+			</h1>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+			<h2>
+				try editing <strong>src/routes/index.svelte</strong>
+			</h2>
 
-	<Counter />
-</section>
+			<Counter />
+		</section>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer" class="drawer-overlay" />
+		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+			<!-- Sidebar content here -->
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
+</div>
 
 <style>
 	section {
