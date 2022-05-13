83889ac736ee91d17e56b321000dba7280959bfd
Braden Wong <bmw02002turbo@gmail.com>
2022-05-13 16:14:47 -0400

    Null types in store

--- src/lib/sessionStore.ts
+++ src/lib/sessionStore.ts
@@ -1,4 +1,4 @@
 import type { Session } from '@supabase/supabase-js';
 import { writable, type Writable } from 'svelte/store';
 
-export const user: Writable<boolean | Session> = writable(false);
+export const user: Writable<boolean | Session | null> = writable(false);
