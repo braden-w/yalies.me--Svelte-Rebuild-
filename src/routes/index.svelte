5422b708f90e84be50b0237d2bd2d8576464c637
Braden Wong <bmw02002turbo@gmail.com>
2022-05-13 14:44:27 -0400

    Index File

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import Auth from '$lib/Auth.svelte';
 </script>
 
 <svelte:head>
@@ -11,46 +11,6 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="drawer">
-	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-side">
-		<label for="my-drawer" class="drawer-overlay" />
-		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-			<!-- Sidebar content here -->
-			<li><a href="summer">Who's in New Haven</a></li>
-			<li><a href="map">Map (Place Yourself on a Map, find Yalies Near Me)</a></li>
-			<li><a href="rides">Ride Share</a></li>
-			<li><a href="meals">Track Meals</a></li>
-			<li><a href="butteries">Butteries</a></li>
-		</ul>
-	</div>
-</div>
+<Auth />
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<style></style>
