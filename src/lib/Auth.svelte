db59c4232ddb4d638cac46f1128bb87b58bfe546
Braden Wong <bmw02002turbo@gmail.com>
2022-05-13 15:49:49 -0400

    Progress on Auth

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -17,22 +17,42 @@
 	};
 </script>
 
-<!-- Add a Google Sign In Button -->
-<button on:click={handleLogin}>
+<button class="btn btn-primary btn-lg btn-block"> Sign in with Google </button>
+
+      <script src="https://accounts.google.com/gsi/client" async defer></script>
+      <div id="g_id_onload"
+         data-client_id="YOUR_GOOGLE_CLIENT_ID"
+         data-login_uri="https://your.domain/your_login_endpoint"
+         data-auto_prompt="false">
+      </div>
+      <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+      </div>
+  </body>
+
+<button
+	type="button"
+	class="  text-white bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+	on:click={handleLogin}
+>
 	<svg
+		class="w-4 h-4 mr-2 -ml-1"
+		aria-hidden="true"
+		focusable="false"
+		data-prefix="fab"
+		data-icon="google"
+		role="img"
 		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		class="feather feather-log-in"
+		viewBox="0 0 488 512"
+		><path
+			fill="currentColor"
+			d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+		/></svg
 	>
-		<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-		<polyline points="10 17 15 12 10 7" />
-		<line x1="15" y1="12" x2="3" y2="12" />
-	</svg>
+	Sign in with Google
 </button>
