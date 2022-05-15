d36de91c797d64b8d6cb883a0909365619ddbc28
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 09:47:41 -0400

    UserMetaData

--- src/lib/sessionStore.ts
+++ src/lib/sessionStore.ts
@@ -1,4 +1,16 @@
 import type { User } from '@supabase/supabase-js';
 import { writable, type Writable } from 'svelte/store';
 
+export interface UserMetadata {
+	avatar_url: string;
+	email: string;
+	email_verified: boolean;
+	full_name: string;
+	iss: string;
+	name: string;
+	picture: string;
+	provider_id: string;
+	sub: string;
+}
+
 export const sessionStore: Writable<User | null> = writable(null);
