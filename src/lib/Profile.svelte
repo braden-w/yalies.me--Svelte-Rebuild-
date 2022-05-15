8fbbfac8bc78dd31850491ae2711492dd558b16e
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 17:46:11 -0400

    Profile Clearing

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -97,103 +97,66 @@
 	</div>
 </div>
 
-<!-- <div class="card"> -->
-<!-- 	<div class="card-title"> -->
-<!-- 		<h1>Profile</h1> -->
-<!-- 	</div> -->
-<!-- 	<div class="card-content"> -->
-<!-- 		<form on:submit={updateProfile}> -->
-<!-- 			<div class="field-row"> -->
-<!-- 				<label for="username">Username</label> -->
-<!-- 				<input -->
-<!-- 					type="text" -->
-<!-- 					name="username" -->
-<!-- 					value={formData?.username || user.username} -->
-<!-- 					on:input={(e: any) => (username = e.target.value)} -->
-<!-- 				/> -->
-<!-- 			</div> -->
-<!-- 			<div class="field-row"> -->
-<!-- 				<label for="website">Website</label> -->
-<!-- 				<input -->
-<!-- 					type="text" -->
-<!-- 					name="website" -->
-<!-- 					value={formData?.website || user.website} -->
-<!-- 					on:input={(e: any) => (website = e.target.value)} -->
-<!-- 				/> -->
-<!-- 			</div> -->
-<!-- 			<div class="field-row"> -->
-<!-- 				<label for="avatar_url">Avatar Url</label> -->
-<!-- 				<input -->
-<!-- 					type="text" -->
-<!-- 					name="avatar_url" -->
-<!-- 					value={formData?.avatar_url || user.avatar_url} -->
-<!-- 					on:
-
-=======
-
-<!-- <div class="card"> -->
-<!-- 	<div class="card-body"> -->
-<!-- 		<h5 class="card-title">User Profile</h5> -->
-<!-- 		<div class="form-group"> -->
-<!-- 			<label for="exampleInputEmail1">Email address</label> -->
-<!-- 			<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model={formData?.email}> -->
-<!-- 			<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
-<!-- 		</div> -->
-<!-- 		<div class="form-group"> -->
-<!-- 			<label for="exampleInputPassword1">Password</label> -->
-<!-- 			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model={formData?.password}> -->
-<!-- 		</div> -->
-<!-- 		<div class="form-group"> -->
-<!-- 			<label for="exampleInputPassword1">Password</label> -->
-<!-- 			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model={formData?.password}> -->
-<!-- 		</div> -->
-<!-- 		<div class="form-group"> -->
-<!-- 			<label for="exampleInputPassword1">Password</label> -->
-<!-- 			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model={formData?.password}> -->
-<!-- 		</div> -->
-<!-- 		<div class="form-group"> -->
-<!-- 			<label for="exampleInputPassword1">Password</label> -->
-<!-- 			<input type="password" class="form-control
-
-
-
-
-<form use:getProfile class="form-widget" on:submit|preventDefault={updateProfile}>
-	<!-- Create an avatar based off formData.photo with formData.full_name as alt text-->
-
-<!-- <v-avatar rounded size="128" class="mt-n4">
-          <v-img
-            :src="userProfileInformation.photo"
-            :alt="userProfileInformation.full_name"
-          />
-        </v-avatar>
-
-        <v-card-title class="mt-n4">
-          {{ userProfileInformation.full_name }}
-        </v-card-title>
-
-        <v-card-subtitle class="mt-n5">
-          {{ userProfileInformation.university }}
-        </v-card-subtitle>
-
-        <div class="text-caption font-weight-light my-n4">
-          {{ userProfileInformation.$state.college }}
-          |
-          {{ userProfileInformation.year }}
-        </div> -->
-
-<!-- <div class="subtitle-3">
-          {{ userProfileInformation.email }}
-        </div> 
-      -->
-<!-- <div>
-	<label for="email">Email</label>
-	<input id="email" type="text" value={$sessionStore.email} disabled />
-</div>
-
-<div class="mt-n4">
-	<label for="full_name">Full Name</label>
-	<input id="full_name" type="text" value={$sessionStore.full_name} />
-</div> -->
-
-<!-- </form> -->
+<ul
+	role="listbox"
+	aria-labelledby="typeahead-0.y481t91gkcn-label"
+	id="typeahead-0.y481t91gkcn-listbox"
+	class="svelte-6c74g2 svelte-typeahead-list"
+>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-0"
+		aria-selected="true"
+		class="svelte-6c74g2 selected"
+	>
+		<div class="py-1 text-sm">Carousel</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-1"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Collapse</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-2"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Progress</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-3"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Radial progress</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-4"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Stat</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-5"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Range</div>
+	</li>
+	<li
+		role="option"
+		id="typeahead-0.y481t91gkcn-result-6"
+		aria-selected="false"
+		class="svelte-6c74g2"
+	>
+		<div class="py-1 text-sm">Drawer</div>
+	</li>
+</ul>
