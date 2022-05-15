4c14dd0fe992562e3676c0471f10ebf9e96941ec
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 18:06:00 -0400

    map

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -3,45 +3,48 @@
 	import 'mapbox-gl/dist/mapbox-gl.css';
 	import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 	import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+	import { onMount } from 'svelte';
 	mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY;
 
 	const NewHaven = { longitude: -72.9, latitude: 41.3 };
 	// longitude = userProfileInformation.location?.longitude ?? NewHaven.longitude;
 	// latitude = userProfileInformation.location?.latitude ?? NewHaven.latitude;
 	// console.log('longitude, latitude:>> ', longitude, latitude)
-	const map = new mapboxgl.Map({
-		container: 'map',
-		style: 'mapbox://styles/mapbox/dark-v10',
-		center: [NewHaven.longitude, NewHaven.latitude],
-		doubleClickZoom: false,
-		zoom: 8,
-		maxZoom: 10
-	});
-
-	/** Add a search bar to the map */
-	const geocoder = new MapboxGeocoder({
-		// * Options here: https://github.com/mapbox/mapbox-gl-geocoder/blob/master/API.md#mapboxgeocoder
-		accessToken: mapboxgl.accessToken,
-		placeholder: 'Jump to location',
-		// proximity: {
-		// 	longitude: longitude,
-		// 	latitude: latitude
-		// },
-		marker: false
-		/*     mapboxgl: mapboxgl, */
-	});
-	map.addControl(geocoder);
-
-	map.on('load', () => {
-		map.flyTo({
-			center: [longitude!, latitude!],
-			speed: 2.5,
-			essential: true
+	onMount(() => {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/dark-v10',
+			center: [NewHaven.longitude, NewHaven.latitude],
+			doubleClickZoom: false,
+			zoom: 8,
+			maxZoom: 10
 		});
-		// loadFacebook(map!, queryYear)
+
+		/** Add a search bar to the map */
+		const geocoder = new MapboxGeocoder({
+			// * Options here: https://github.com/mapbox/mapbox-gl-geocoder/blob/master/API.md#mapboxgeocoder
+			accessToken: mapboxgl.accessToken,
+			placeholder: 'Jump to location',
+			// proximity: {
+			// 	longitude: longitude,
+			// 	latitude: latitude
+			// },
+			marker: false
+			/*     mapboxgl: mapboxgl, */
+		});
+		map.addControl(geocoder);
+
+		map.on('load', () => {
+			map.flyTo({
+				// center: [longitude!, latitude!],
+				speed: 2.5,
+				essential: true
+			});
+			// loadFacebook(map!, queryYear)
+		});
+		// Create a marker on the map that the user can move around
+		// new mapboxgl.Marker().setLngLat([coords.value.longitude, coords.value.latitude]).addTo(map)
 	});
-	// Create a marker on the map that the user can move around
-	// new mapboxgl.Marker().setLngLat([coords.value.longitude, coords.value.latitude]).addTo(map)
 </script>
 
 <svelte:head>
