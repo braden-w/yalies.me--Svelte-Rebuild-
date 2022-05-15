5c9024b6c5df6ea9fc28d5dff4afadbcfa625302
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 17:01:09 -0400

    Text Sizes

--- src/lib/LocationAutoComplete.svelte
+++ src/lib/LocationAutoComplete.svelte
@@ -49,7 +49,7 @@
 
 <!-- A Location Autocomplete built with DaisyUI that uses the Google Map Places API to autocomplete the location as the user is typing -->
 
-<label class="block text-gray-700 text-sm font-bold mb-2" for="location">Location</label>
+<label class="block text-sm font-bold mb-2" for="location">Location</label>
 <input
 	type="text"
 	class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
