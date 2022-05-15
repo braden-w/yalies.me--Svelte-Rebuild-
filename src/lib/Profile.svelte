1412f611f9355e7a59e0bdf1803efa3284d22130
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 15:35:33 -0400

    Location AutoComplete

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -68,20 +68,13 @@
 
 <!-- Put a centered card on the screen. Inside it, there are multiple labelled inputs that are binded to the corresponding properties of the user -->
 <div class="w-full max-w-md mx-auto">
-	<LocationAutoComplete />
 	<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 		<div class="mb-4">
 			<h1 class="text-2xl font-bold">Profile</h1>
 			<p class="text-lg">Edit your profile</p>
 		</div>
 		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="username">Location</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				id="username"
-				type="text"
-				placeholder="Username"
-			/>
+			<LocationAutoComplete />
 		</div>
 		<div class="mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="website"> Website </label>
