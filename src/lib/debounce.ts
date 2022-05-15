d2e0584bc22517b30c577db210e977e350299718
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 14:30:36 -0400

    Remove Debounce

--- src/lib/debounce.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (callback: () => void, delay = 300): (() => void) => {
-	let timeout: NodeJS.Timeout | undefined;
-	return () => {
-		clearTimeout(timeout);
-		timeout = setTimeout(() => callback(), delay);
-	};
-};
