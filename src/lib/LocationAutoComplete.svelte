be5f1fe70591effb4b41cf62e36ac874baa869db
Braden Wong <bmw02002turbo@gmail.com>
2022-05-15 12:09:22 -0400

    LocationAutoComplete: Scaffold

---
+++ src/lib/LocationAutoComplete.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let query = '';
+	let loading = false;
+	let results = [];
+	let selected = null;
+	let selectedIndex = -1;
+	let selectedLocation = null;
+	let selectedLocationIndex = -1;
+
+
+</script>
+
+<!-- A Location Autocomplete built with DaisyUI that uses the Google Map Places API to autocomplete the location as the user is typing -->
+
+<input type="text" id="location" placeholder="Location" bind:value={query} />
