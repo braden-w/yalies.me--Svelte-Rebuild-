3154c0c95819253d5cb41604a1614bda0abcc27b
Braden Wong <bmw02002turbo@gmail.com>
2022-05-16 18:25:29 -0400

    signIn functions

--- src/lib/utils/sessionStore.ts
+++ src/lib/utils/sessionStore.ts
@@ -1,4 +1,36 @@
+import {supabase} from '$lib/utils/supabaseClient';
+import type {ApiError} from '@supabase/supabase-js';
 import {writable, type Writable} from 'svelte/store';
 import type {SessionStore} from 'types/SessionStore';
 
 export const sessionStore: Writable<SessionStore | null> = writable(null);
+
+export let loading = true;
+
+export const signIn = async () => {
+	try {
+		loading = true;
+		const {error} = await supabase.auth.signIn({provider: 'google'});
+		if (error) throw error;
+	} catch (error) {
+		if ((error as ApiError).message) {
+			alert((error as ApiError).message);
+		}
+	} finally {
+		loading = false;
+	}
+};
+
+export async function signOut() {
+	try {
+		loading = true;
+		const {error} = await supabase.auth.signOut();
+		if (error) throw error;
+	} catch (error) {
+		if ((error as ApiError).message) {
+			alert((error as ApiError).message);
+		}
+	} finally {
+		loading = false;
+	}
+}
