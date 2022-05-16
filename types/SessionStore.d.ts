d03599659ea6718ac76ec8aeaadb06e79a2e4e5c
Braden Wong <bmw02002turbo@gmail.com>
2022-05-16 17:34:31 -0400

    SessionStore

---
+++ types/SessionStore.d.ts
@@ -0,0 +1,14 @@
+export interface SessionStore {
+  avatar_url: string;
+  email: string;
+  email_verified: boolean;
+  full_name: string;
+  iss: string;
+  name: string;
+  picture: string;
+  provider_id: string;
+  sub: string;
+  id: string | undefined;
+  user_response_id: string;
+
+}
