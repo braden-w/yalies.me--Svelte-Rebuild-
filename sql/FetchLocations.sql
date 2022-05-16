86dd8b2643d58156a8c0635be639d983738fec3a
Braden Wong <bmw02002turbo@gmail.com>
2022-05-16 12:43:23 -0400

    Fix: Fetch_locations

--- sql/FetchLocations.sql
+++ sql/FetchLocations.sql
@@ -2,12 +2,12 @@
 -- Place people is a list of users that are associated with the place
 -- Each user has a id, name, and avatar_url
 
-select description, lat, lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) from places 
+select places.description, places.lat, places.lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) as people from places 
 
 
 CREATE OR REPLACE FUNCTION fetch_locations() RETURNS TABLE(description varchar, lat double precision, lng double precision, people json)
   AS $$ 
   BEGIN
-  RETURN QUERY SELECT description, lat, lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) from places;
+  RETURN QUERY SELECT places.description, places.lat, places.lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) as people from places;
   END;
   $$ LANGUAGE plpgsql;
