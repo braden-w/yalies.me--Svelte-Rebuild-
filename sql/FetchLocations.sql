dc49e787d439fa29186455256607e7959a8b815b
Braden Wong <bmw02002turbo@gmail.com>
2022-05-16 15:23:08 -0400

    Formatting functions

--- sql/FetchLocations.sql
+++ sql/FetchLocations.sql
@@ -1,13 +1,57 @@
 -- List all places from the database. Return it as a list of items that contains place description, place lat, place lng, and place people
 -- Place people is a list of users that are associated with the place
 -- Each user has a id, name, and avatar_url
+select
+  places.description,
+  places.lat,
+  places.lng,
+  (
+    select
+      json_agg(
+        json_build_object(
+          'id',
+          users.id,
+          'name',
+          users.name,
+          'avatar_url',
+          users.avatar_url
+        )
+      )
+    from
+      users
+  ) as people
+from
+  places;
 
-select places.description, places.lat, places.lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) as people from places 
+CREATE
+OR REPLACE FUNCTION fetch_locations() RETURNS TABLE(
+  description varchar,
+  lat double precision,
+  lng double precision,
+  people json
+) AS $ $ BEGIN RETURN QUERY
+SELECT
+  places.description,
+  places.lat,
+  places.lng,
+  (
+    select
+      json_agg(
+        json_build_object(
+          'id',
+          users.id,
+          'name',
+          users.name,
+          'avatar_url',
+          users.avatar_url
+        )
+      )
+    from
+      users
+  ) as people
+from
+  places;
 
+END;
 
-CREATE OR REPLACE FUNCTION fetch_locations() RETURNS TABLE(description varchar, lat double precision, lng double precision, people json)
-  AS $$ 
-  BEGIN
-  RETURN QUERY SELECT places.description, places.lat, places.lng, (select json_agg(json_build_object('id', users.id, 'name', users.name, 'avatar_url', users.avatar_url)) from users) as people from places;
-  END;
-  $$ LANGUAGE plpgsql;
+$ $ LANGUAGE plpgsql;
