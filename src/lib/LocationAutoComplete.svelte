544a84f44cfa736841554d0f5ec08d12f39f7640
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 00:11:57 -0400

    Import

--- src/lib/LocationAutoComplete.svelte
+++ src/lib/LocationAutoComplete.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { supabase } from '$lib/utils/supabaseClient';
 	import { sessionStore } from '$lib/utils/sessionStore';
-	import type { UserMetadata } from 'types/UserMetaData';
 
 	let query = '';
 	let loading = false;
