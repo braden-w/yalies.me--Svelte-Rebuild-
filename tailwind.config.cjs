451530bd50dc95e9bfedfcdecf7035094cd3e8ba
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 10:50:43 -0400

    Feat: Yale Theme Extends

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  safelist: [{ pattern: /bg-.+-.+/ }],
+  safelist: [{pattern: /bg-.+-.+/}],
 
   theme: {
     extend: {}
@@ -9,6 +9,22 @@ const config = {
 
   daisyui: {
     themes: [
+      {
+        yale: {
+          primary: "#00356b",
+          "primary-content": "#ffffff",
+          secondary: "#7D919B",
+          "secondary-content": "#ffffff",
+          accent: "#EB6B47",
+          "accent-content": "#163835",
+          neutral: "#3d4451",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
+        },
+      },
       'light',
       'dark',
       'cupcake',
