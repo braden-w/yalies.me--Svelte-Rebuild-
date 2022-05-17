21ab7b416a6110f342814bfb9fe9e30a77cbf404
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 11:34:58 -0400

    UsersToPlaces: create

---
+++ sql/UsersToPlaces.sql
@@ -0,0 +1,10 @@
+CREATE VIEW users_to_places AS
+SELECT
+  users.name,
+  users.avatar_url,
+  places.place_id,
+  places.description
+FROM
+  users
+  LEFT JOIN user_responses ON users.user_response_id = user_responses.user_response_id
+  LEFT JOIN places on places.place_id = user_responses.place_id
