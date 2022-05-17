7c75f6c0d93d507864d734679cf08646b389c289
Braden Wong <bmw02002turbo@gmail.com>
2022-05-16 21:30:11 -0400

    Notifications

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -111,7 +111,7 @@
 										stroke-linecap="round"
 										stroke-linejoin="round"
 										stroke-width="2"
-										d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+										d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
 									/></svg
 								>
 								<span class="badge badge-sm indicator-item">8</span>
@@ -122,7 +122,7 @@
 							class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
 						>
 							<div class="card-body">
-								<span class="font-bold text-lg">8 Items</span>
+								<span class="font-bold text-lg">Notifications</span>
 								<span class="text-info">Subtotal: $999</span>
 								<div class="card-actions">
 									<button class="btn btn-primary btn-block">View cart</button>
