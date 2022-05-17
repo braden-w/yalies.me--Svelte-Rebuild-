fb64a8a6685038615d5f297bcb6e6cd5597c8c8d
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 18:43:23 -0400

    Attempt to use $: syntax

--- src/lib/LocationAutoComplete.svelte
+++ src/lib/LocationAutoComplete.svelte
@@ -2,227 +2,16 @@
   import { supabase } from '$lib/utils/supabaseClient';
   import { sessionStore } from '$lib/utils/sessionStore';
   import type { definitions } from '$lib/supabase';
+  import { defaultResults } from '$lib/LocationAutoComplete';
 
   let query = '';
+  $: handleQueryChange(query);
+  let focused = false;
   let loading = false;
-  let results: google.maps.places.AutocompletePrediction[] = [];
-  const defaultResults = [
-    {
-      description: 'New Haven, CT, USA',
-      matched_substrings: [
-        {
-          length: 9,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJ5XCAOkTY54kR7WSyWcZUo_Y',
-      structured_formatting: {
-        main_text: 'New Haven',
-        main_text_matched_substrings: [
-          {
-            length: 9,
-            offset: 0
-          }
-        ],
-        secondary_text: 'CT, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'New Haven'
-        },
-        {
-          offset: 11,
-          value: 'CT'
-        },
-        {
-          offset: 15,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    },
-    {
-      description: 'New York, NY, USA',
-      matched_substrings: [
-        {
-          length: 8,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJOwg_06VPwokRYv534QaPC8g',
-      structured_formatting: {
-        main_text: 'New York',
-        main_text_matched_substrings: [
-          {
-            length: 8,
-            offset: 0
-          }
-        ],
-        secondary_text: 'NY, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'New York'
-        },
-        {
-          offset: 10,
-          value: 'NY'
-        },
-        {
-          offset: 14,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    },
-    {
-      description: 'Los Angeles, CA, USA',
-      matched_substrings: [
-        {
-          length: 8,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJE9on3F3HwoAR9AhGJW_fL-I',
-      structured_formatting: {
-        main_text: 'Los Angeles',
-        main_text_matched_substrings: [
-          {
-            length: 8,
-            offset: 0
-          }
-        ],
-        secondary_text: 'CA, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'Los Angeles'
-        },
-        {
-          offset: 13,
-          value: 'CA'
-        },
-        {
-          offset: 17,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    },
-    {
-      description: 'San Francisco, CA, USA',
-      matched_substrings: [
-        {
-          length: 8,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJIQBpAG2ahYAR_6128GcTUEo',
-      structured_formatting: {
-        main_text: 'San Francisco',
-        main_text_matched_substrings: [
-          {
-            length: 8,
-            offset: 0
-          }
-        ],
-        secondary_text: 'CA, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'San Francisco'
-        },
-        {
-          offset: 15,
-          value: 'CA'
-        },
-        {
-          offset: 19,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    },
-    {
-      description: 'Boston, MA, USA',
-      matched_substrings: [
-        {
-          length: 6,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJGzE9DS1l44kRoOhiASS_fHg',
-      structured_formatting: {
-        main_text: 'Boston',
-        main_text_matched_substrings: [
-          {
-            length: 6,
-            offset: 0
-          }
-        ],
-        secondary_text: 'MA, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'Boston'
-        },
-        {
-          offset: 8,
-          value: 'MA'
-        },
-        {
-          offset: 12,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    },
-    {
-      description: 'Washington D.C., DC, USA',
-      matched_substrings: [
-        {
-          length: 10,
-          offset: 0
-        }
-      ],
-      place_id: 'ChIJW-T2Wt7Gt4kRKl2I1CJFUsI',
-      reference: 'ChIJW-T2Wt7Gt4kRKl2I1CJFUsI',
-      structured_formatting: {
-        main_text: 'Washington D.C.',
-        main_text_matched_substrings: [
-          {
-            length: 10,
-            offset: 0
-          }
-        ],
-        secondary_text: 'DC, USA'
-      },
-      terms: [
-        {
-          offset: 0,
-          value: 'Washington D.C.'
-        },
-        {
-          offset: 17,
-          value: 'DC'
-        },
-        {
-          offset: 21,
-          value: 'USA'
-        }
-      ],
-      types: ['locality', 'political', 'geocode']
-    }
-  ];
+  let results: google.maps.places.AutocompletePrediction[] = defaultResults;
   function resetResults() {
     results = defaultResults;
   }
-  resetResults();
 
   $: isQueryLongEnough = query.length > 2;
   supabase
@@ -245,7 +34,7 @@
 
   // When query changes value
   let timer: NodeJS.Timeout | null;
-  function handleQueryChange() {
+  function handleQueryChange(query: string) {
     if (query.length == 0) {
       const user_response_id = $sessionStore?.user_response_id;
       supabase.from('user_responses').upsert(
@@ -255,9 +44,8 @@
         }
       );
     }
-    if (!isQueryLongEnough) {
-      resetResults();
-    } else {
+    if (!isQueryLongEnough) return resetResults();
+    else {
       if (timer) clearTimeout(timer);
       timer = setTimeout(() => {
         fetchResults();
@@ -349,7 +137,7 @@
 
 <!-- A Location Autocomplete built with DaisyUI that uses the Google Map Places API to autocomplete the location as the user is typing -->
 
-<div class="dropdown dropdown-top w-full">
+<div class="dropdown dropdown-top w-full" class:dropdown-open={focused}>
   <div class="form-control">
     <label class="label" for="location">
       <span class="label-text">I'm currently in...</span>
@@ -361,7 +149,6 @@
       class="input input-bordered"
       placeholder="Start typing your city here..."
       bind:value={query}
-      on:input={handleQueryChange}
     />
   </div>
 
