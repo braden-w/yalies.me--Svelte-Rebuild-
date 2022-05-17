a56e51a0e0712184b74faab2b78863dd59222b47
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 10:49:28 -0400

    Yale Select Theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -176,6 +176,30 @@
               <div class="grid grid-cols-1 gap-3 p-3" tabindex="0">
                 <div
                   class="outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
+                  data-set-theme="yale"
+                  data-act-class="outline"
+                >
+                  <div
+                    data-theme="yale"
+                    class="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+                  >
+                    <div class="grid grid-cols-5 grid-rows-3">
+                      <div
+                        class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4"
+                      >
+                        <div class="flex-grow text-sm font-bold">yale</div>
+                        <div class="flex flex-shrink-0 flex-wrap gap-1">
+                          <div class="bg-primary w-2 rounded" />
+                          <div class="bg-secondary w-2 rounded" />
+                          <div class="bg-accent w-2 rounded" />
+                          <div class="bg-neutral w-2 rounded" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
                   data-set-theme="light"
                   data-act-class="outline"
                 >
