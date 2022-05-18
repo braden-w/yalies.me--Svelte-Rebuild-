b42a352a559b2e23d335b0c9d72115533ea0dcfb
Braden Wong <bmw02002turbo@gmail.com>
2022-05-17 21:09:04 -0400

    Formatting

--- src/lib/utils/facebookToPlaceID.ts
+++ src/lib/utils/facebookToPlaceID.ts
@@ -1,18 +1,17 @@
 import type {Payload} from "$lib/LocationAutoComplete";
-import {definitions} from "$lib/supabase";
+import type {definitions} from "$lib/supabase";
 import {supabase} from "$lib/utils/supabaseClient";
 
-async function getPlaceIdFromFacebook(){
-const {data} = await supabase.from<definitions['facebook']>('facebook').select('*');
-const firstResult = data[0];
-firstResult.
+async function getPlaceIdFromFacebook() {
+  const {data} = await supabase.from<definitions['facebook']>('facebook').select('*');
+  const firstResult = data?.[0];
 }
 
 
 
 
 export async function uploadPlaceToSupabase(payload: Payload) {
-  const { error: errorPlaces } = await supabase.from('places').upsert(payload, {
+  const {error: errorPlaces} = await supabase.from('places').upsert(payload, {
     returning: 'minimal' // Don't return the value after inserting
   });
   if (errorPlaces) throw errorPlaces;
@@ -22,8 +21,8 @@ export async function uploadUserPlaceSelectionToSupabase(
   place_id: string
 ) {
   if (!user_response_id) return;
-  const { error: errorUserIDtoPlaceID } = await supabase
+  const {error: errorUserIDtoPlaceID} = await supabase
     .from('user_responses')
-    .upsert({ place_id, user_response_id }, { returning: 'minimal' });
+    .upsert({place_id, user_response_id}, {returning: 'minimal'});
   if (errorUserIDtoPlaceID) throw errorUserIDtoPlaceID;
 }
