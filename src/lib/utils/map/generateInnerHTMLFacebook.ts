ba48ab0bea9a9090ff4040d81f7659b4bd2a6557
Braden Wong <bmw02002turbo@gmail.com>
2022-05-20 00:22:15 -0700

    generateInnerHTMLFacebook: Copy new innerHTML Function

--- src/lib/utils/map/generateInnerHTMLFacebook.ts
+++ src/lib/utils/map/generateInnerHTMLFacebook.ts
@@ -1,53 +1,61 @@
-import type { FetchedLocation, Person } from '$lib/types/FetchedLocation';
 import type { definitions } from '$lib/types/supabase';
 
 interface Person {
-  email: string
-  first_name: string
-  middle_name: string
-  last_name: string
-  image: string
-  year: number
+  email: string;
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  image: string;
+  year: number;
 }
 
-export function generateInnerHTMLFacebook(
-  facebookToPlace: definitions['facebook_to_places']
-): string {
-  // Get 3 random people from the 'people' property of facebookToPlace
-  const people = facebookToPlace.people as Person[];
-  const shuffledPeople = sort(() => 0.5 - Math.random());
+export function generateInnerHTML(place: definitions['places_with_facebook']) {
+  // Get 3 random people from the 'people' property of placeWithPeople
+  const people = place.people as unknown as Person[];
+  /**The first three people who will be the icons in the stack on the map */
+  const stackIcons = people.sort(() => 0.5 - Math.random()).slice(0, 3);
+  const { place_id, description } = place;
 
-  // Three cases for the number of people in the facebookToPlace
-  if (shuffledPeople.length === 0) return '';
+  // Three cases for the number of people in the placeWithPeople
+  if (stackIcons.length === 0) return '';
   return `<div class="dropdown dropdown-hover">
-  ${stackOfIcons(shuffledPeople, facebookToPlace)}
-  ${listOfPeopleOnHover(shuffledPeople, facebookToPlace)}
+  ${generateStackOfIcons({ threePeople: stackIcons, indicator: people.length })}
+  ${generateHover({
+    numberOfIconsStacked: stackIcons.length,
+    place_id: place_id ?? '',
+    description: description ?? '',
+    people
+  })}
 </div>`;
 }
 
-function stackOfIcons(
-  shuffledPeople: Person[],
-  facebookToPlace: FetchedLocation
-): string {
+/**Generates a stack of icons with three random people and an indicator in the top right for overall number of people at a location */
+function generateStackOfIcons({
+  threePeople,
+  indicator
+}: {
+  threePeople: Person[];
+  indicator: number;
+}): string {
   return `<label tabindex="0" name="selected" class="stack">
     <div class="avatar indicator">
       <span class="indicator-item badge badge-secondary"
-        >${facebookToPlace.people.length}</span
+        >${indicator}</span
       >
       <div class="w-8 h-8 rounded-lg outline-on-click">
         <img
-          src="${facebookToPlace.people[0].avatar_url}"
+          src="${threePeople[0].avatar_url}"
           referrerpolicy="no-referrer"
         />
       </div>
     </div>
     ${
-      shuffledPeople.length >= 2
+      threePeople.length >= 2
         ? `
     <div class="avatar">
       <div class="w-8 h-8 rounded-lg outline-on-click">
         <img
-          src="${facebookToPlace.people[1].avatar_url}"
+          src="${threePeople[1].avatar_url}"
           referrerpolicy="no-referrer"
         />
       </div>
@@ -55,12 +63,12 @@ function stackOfIcons(
     `
         : ''
     } ${
-    shuffledPeople.length >= 3
+    threePeople.length >= 3
       ? `
     <div class="avatar">
       <div class="w-8 h-8 rounded-lg outline-on-click">
         <img
-          src="${facebookToPlace.people[2].avatar_url}"
+          src="${threePeople[2].avatar_url}"
           referrerpolicy="no-referrer"
         />
       </div>
@@ -72,25 +80,34 @@ function stackOfIcons(
 }
 
 /**Generates the dropdown menu that is created when you hover on a component */
-function listOfPeopleOnHover(
-  shuffledPeople: Person[],
-  facebookToPlace: FetchedLocation
-): string {
+function generateHover({
+  numberOfIconsStacked,
+  place_id,
+  description,
+  people
+}: {
+  numberOfIconsStacked: number;
+  place_id: string;
+  description: string;
+  people: Person[];
+}): string {
   const placeTitle = `<li>
-      <a class="justify-between" href="/places/${facebookToPlace.place_id}">
-        ${facebookToPlace.description}
+      <a class="justify-between" href="/places/${place_id}">
+        ${description}
       </a>
     </li>`;
   return `<ul tabindex="0" class="menu menu-compact dropdown-content mt-${
-    shuffledPeople.length > 3 ? '3' : shuffledPeople.length
+    numberOfIconsStacked > 3 ? '3' : numberOfIconsStacked
   } p-2 shadow bg-base-100 rounded-box w-52">
     ${placeTitle} 
-    ${facebookToPlace.people.map(personToListItem).join('')}
+    ${peopleToListItems(people)}
   </ul>`;
 }
 
-function personToListItem(person: Person): string {
-  return `<li>
+function peopleToListItems(people: Person[]): string {
+  return people
+    .map(
+      (person) => `<li>
       <a class="content-center" href="/users/${person.id}">
         <div class="avatar">
           <div class="w-8 rounded-lg">
@@ -99,5 +116,7 @@ function personToListItem(person: Person): string {
         </div>
         <span class="text-xs">${person.name}</span>
       </a>
-    </li>`;
+    </li>`
+    )
+    .join('');
 }
