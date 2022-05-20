bbbe461c3dddbbb857c555fed38e90fdb04e703e
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 22:15:40 -0700

    Fix: Array types

--- src/lib/stores/map/facebook.ts
+++ src/lib/stores/map/facebook.ts
@@ -2,12 +2,12 @@ import type {definitions} from '$lib/supabase';
 import {supabase} from '$lib/utils/supabaseClient';
 import { writable, type Writable } from 'svelte/store';
 
-export const facebook: Writable<definitions['facebook_to_places'] | null> = writable(null);
+export const facebook: Writable<definitions['facebook_to_places'][] | null> = writable(null);
 
 export async function load() {
     const { data } = await supabase
       .from<definitions['facebook_to_places']>('facebook_to_places')
       .select('*')
       .not('year', 'is', null);
-	facebook.set(data);
+facebook.set(data);
 }
