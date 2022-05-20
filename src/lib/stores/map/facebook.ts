801eeb4de4b81ebb3702f5232d8556597b291389
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 22:19:46 -0700

    Facebook: Load Facebook

--- src/lib/stores/map/facebook.ts
+++ src/lib/stores/map/facebook.ts
@@ -1,13 +1,14 @@
-import type {definitions} from '$lib/supabase';
-import {supabase} from '$lib/utils/supabaseClient';
+import type { definitions } from '$lib/supabase';
+import { supabase } from '$lib/utils/supabaseClient';
 import { writable, type Writable } from 'svelte/store';
 
-export const facebook: Writable<definitions['facebook_to_places'][] | null> = writable(null);
+export const facebook: Writable<definitions['facebook_to_places'][] | null> =
+  writable(null);
 
 export async function load() {
-    const { data } = await supabase
-      .from<definitions['facebook_to_places']>('facebook_to_places')
-      .select('*')
-      .not('year', 'is', null);
-facebook.set(data);
+  const { data } = await supabase
+    .from<definitions['facebook_to_places']>('facebook_to_places')
+    .select('*')
+    .not('year', 'is', null);
+  facebook.set(data);
 }
