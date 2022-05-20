08008036f711730e1a365f3b0876aec8a7b229ad
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 22:23:45 -0700

    Formatting InnerHTML

--- src/lib/utils/map/generateInnerHTML.ts
+++ src/lib/utils/map/generateInnerHTML.ts
@@ -7,62 +7,73 @@ export function generateInnerHTML(fetchedLocation: FetchedLocation) {
   // Three cases for the number of people in the fetchedLocation
   if (shuffledPeople.length === 0) return '';
   return `<div class="dropdown dropdown-hover">
-				<label tabindex="0" name="selected" class="stack">
-					<div class="avatar indicator">
-						<span class="indicator-item badge badge-secondary">${
-              fetchedLocation.people.length
-            }</span>
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${
-                fetchedLocation.people[0].avatar_url
-              }" referrerpolicy="no-referrer"/>
-						</div>
-					</div>
-					${
-            shuffledPeople.length >= 2
-              ? `<div class="avatar">
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${fetchedLocation.people[1].avatar_url}" referrerpolicy="no-referrer"/>
-						</div>
-					</div>`
-              : ''
-          }
-					${
-            shuffledPeople.length >= 3
-              ? `<div class="avatar">
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${fetchedLocation.people[2].avatar_url}" referrerpolicy="no-referrer"/>
-						</div>
-					</div>`
-              : ''
-          }
-				</label>
-				<ul
-					tabindex="0"
-					class="menu menu-compact dropdown-content mt-${
-            shuffledPeople.length > 3 ? '3' : shuffledPeople.length
-          } p-2 shadow bg-base-100 rounded-box w-52"
-				>
-					<li>
-						<a class="justify-between" href="/places/${fetchedLocation.place_id}">${
-    fetchedLocation.description
-  }</a>
-					</li>
-							${fetchedLocation.people
-                .map(
-                  (person) => `
-								<li>
-									<a class="content-center" href="/users/${person.id}">
-										<div class="avatar">
-											<div class="w-8 rounded-lg">
-												<img src="${person.avatar_url}" referrerpolicy="no-referrer"/>
-											</div>
-										</div> 
-										<span class="text-xs">${person.name}</span>
-									</a>
-								</li>`
-                )
-                .join('')}
-				</ul>
-				</div>`;
+  <label tabindex="0" name="selected" class="stack">
+    <div class="avatar indicator">
+      <span class="indicator-item badge badge-secondary"
+        >${fetchedLocation.people.length}</span
+      >
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[0].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    ${
+      shuffledPeople.length >= 2
+        ? `
+    <div class="avatar">
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[1].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    `
+        : ''
+    } ${
+    shuffledPeople.length >= 3
+      ? `
+    <div class="avatar">
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[2].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    `
+      : ''
+  }
+  </label>
+  <ul
+    tabindex="0"
+    class="menu menu-compact dropdown-content mt-${
+      shuffledPeople.length > 3 ? '3' : shuffledPeople.length
+    } p-2 shadow bg-base-100 rounded-box w-52"
+  >
+    <li>
+      <a class="justify-between" href="/places/${fetchedLocation.place_id}"
+        >${fetchedLocation.description}</a
+      >
+    </li>
+    ${fetchedLocation.people
+      .map(
+        (person) => `
+    <li>
+      <a class="content-center" href="/users/${person.id}">
+        <div class="avatar">
+          <div class="w-8 rounded-lg">
+            <img src="${person.avatar_url}" referrerpolicy="no-referrer" />
+          </div>
+        </div>
+        <span class="text-xs">${person.name}</span>
+      </a>
+    </li>
+    `
+      )
+      .join('')}
+  </ul>
+</div>`;
 }
