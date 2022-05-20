3e73fbc96947f9936b4d3ee8554517f2e2b727ad
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 23:35:48 -0700

    Consolidate Imports

--- src/lib/utils/map/generateInnerHTML.ts
+++ src/lib/utils/map/generateInnerHTML.ts
@@ -1,6 +1,4 @@
-import type { FetchedLocation } from '$lib/types/FetchedLocation';
-import { stackOfIcons } from '$lib/utils/map/generateInnerHTML/stackOfIcons';
-import { listOfPeopleOnHover } from './generateInnerHTML/listOfPeopleOnHover';
+import type { FetchedLocation, Person } from '$lib/types/FetchedLocation';
 
 export function generateInnerHTML(fetchedLocation: FetchedLocation) {
   // Get 3 random people from the 'people' property of fetchedLocation
@@ -12,4 +10,81 @@ export function generateInnerHTML(fetchedLocation: FetchedLocation) {
   ${stackOfIcons(shuffledPeople, fetchedLocation)}
   ${listOfPeopleOnHover(shuffledPeople, fetchedLocation)}
 </div>`;
+}
+
+function stackOfIcons(
+  shuffledPeople: Person[],
+  fetchedLocation: FetchedLocation
+): string {
+  return `<label tabindex="0" name="selected" class="stack">
+    <div class="avatar indicator">
+      <span class="indicator-item badge badge-secondary"
+        >${fetchedLocation.people.length}</span
+      >
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[0].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    ${
+      shuffledPeople.length >= 2
+        ? `
+    <div class="avatar">
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[1].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    `
+        : ''
+    } ${
+    shuffledPeople.length >= 3
+      ? `
+    <div class="avatar">
+      <div class="w-8 h-8 rounded-lg outline-on-click">
+        <img
+          src="${fetchedLocation.people[2].avatar_url}"
+          referrerpolicy="no-referrer"
+        />
+      </div>
+    </div>
+    `
+      : ''
+  }
+  </label>`;
+}
+
+/**Generates the dropdown menu that is created when you hover on a component */
+function listOfPeopleOnHover(
+  shuffledPeople: Person[],
+  fetchedLocation: FetchedLocation
+): string {
+  const placeTitle = `<li>
+      <a class="justify-between" href="/places/${fetchedLocation.place_id}">
+        ${fetchedLocation.description}
+      </a>
+    </li>`;
+  return `<ul tabindex="0" class="menu menu-compact dropdown-content mt-${
+    shuffledPeople.length > 3 ? '3' : shuffledPeople.length
+  } p-2 shadow bg-base-100 rounded-box w-52">
+    ${placeTitle} 
+    ${fetchedLocation.people.map(personToListItem).join('')}
+  </ul>`;
+}
+
+function personToListItem(person: Person): string {
+  return `<li>
+      <a class="content-center" href="/users/${person.id}">
+        <div class="avatar">
+          <div class="w-8 rounded-lg">
+            <img src="${person.avatar_url}" referrerpolicy="no-referrer" />
+          </div>
+        </div>
+        <span class="text-xs">${person.name}</span>
+      </a>
+    </li>`;
 }
