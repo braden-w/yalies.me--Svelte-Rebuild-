83d1fa3a7862cbe1cdea112ee180facaa0b44030
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 22:22:06 -0700

    Import generateInnerHTML

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,6 +24,7 @@
 
   import { onMount } from 'svelte';
   import { supabase } from '$lib/utils/supabaseClient';
+  import { generateInnerHTML } from '$lib/utils/map/generateInnerHTML';
 
   mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY;
 
@@ -67,74 +68,6 @@
       // loadFacebook(map!, queryYear)
     });
 
-    function generateInnerHTML(fetchedLocation: FetchedLocation) {
-      // Get 3 random people from the 'people' property of fetchedLocation
-      const shuffledPeople = fetchedLocation.people.sort(
-        () => 0.5 - Math.random()
-      );
-
-      // Three cases for the number of people in the fetchedLocation
-      if (shuffledPeople.length === 0) return '';
-      return `<div class="dropdown dropdown-hover">
-				<label tabindex="0" name="selected" class="stack">
-					<div class="avatar indicator">
-						<span class="indicator-item badge badge-secondary">${
-              fetchedLocation.people.length
-            }</span>
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${
-                fetchedLocation.people[0].avatar_url
-              }" referrerpolicy="no-referrer"/>
-						</div>
-					</div>
-					${
-            shuffledPeople.length >= 2
-              ? `<div class="avatar">
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${fetchedLocation.people[1].avatar_url}" referrerpolicy="no-referrer"/>
-						</div>
-					</div>`
-              : ''
-          }
-					${
-            shuffledPeople.length >= 3
-              ? `<div class="avatar">
-						<div class="w-8 h-8 rounded-lg outline-on-click">
-							<img src="${fetchedLocation.people[2].avatar_url}" referrerpolicy="no-referrer"/>
-						</div>
-					</div>`
-              : ''
-          }
-				</label>
-				<ul
-					tabindex="0"
-					class="menu menu-compact dropdown-content mt-${
-            shuffledPeople.length > 3 ? '3' : shuffledPeople.length
-          } p-2 shadow bg-base-100 rounded-box w-52"
-				>
-					<li>
-						<a class="justify-between" href="/places/${fetchedLocation.place_id}">${
-        fetchedLocation.description
-      }</a>
-					</li>
-							${fetchedLocation.people
-                .map(
-                  (person) => `
-								<li>
-									<a class="content-center" href="/users/${person.id}">
-										<div class="avatar">
-											<div class="w-8 rounded-lg">
-												<img src="${person.avatar_url}" referrerpolicy="no-referrer"/>
-											</div>
-										</div> 
-										<span class="text-xs">${person.name}</span>
-									</a>
-								</li>`
-                )
-                .join('')}
-				</ul>
-				</div>`;
-    }
     fetchedLocations.forEach((fetchedLocation) => {
       const el = document.createElement('div');
       el.className = 'marker';
