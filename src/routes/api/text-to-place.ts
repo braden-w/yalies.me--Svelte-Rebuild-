ca8c32e17281a4a953bc9eb08389ace1f40b040f
Braden Wong <bmw02002turbo@gmail.com>
2022-05-19 21:01:08 -0700

    Prettify text-to-place

--- src/routes/api/text-to-place.ts
+++ src/routes/api/text-to-place.ts
@@ -1,54 +1,59 @@
-import type {Payload} from "$lib/LocationAutoComplete";
-import type {definitions} from "$lib/supabase";
-import {supabase} from "$lib/utils/supabaseClient";
+import type { Payload } from '$lib/LocationAutoComplete';
+import type { definitions } from '$lib/supabase';
+import { supabase } from '$lib/utils/supabaseClient';
 
 interface TextToPlaceResponse {
-  place_id: string | null,
-    lat: number | null,
-    lng: number | null,
+  place_id: string | null;
+  lat: number | null;
+  lng: number | null;
+}
+
+async function geocodeAddress(address: string) {
+  try {
+    const requestString = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${import.meta.env.VITE_GOOGLE_MAP_KEY_SERVER}`;
+    const response = await fetch(requestString);
+    const data = await response.json();
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
 }
 
 async function getPlaceIdFromFacebook() {
-  const {data} = await supabase.from<definitions['facebook']>('facebook').select('*');
-  const firstResult = data?.[0];
+  const { data } = await supabase
+    .from<definitions['facebook']>('facebook')
+    .select('*')
+    .eq('year', '2022');
+  return { data };
 }
 
-async function textAddressToPlace(address: string): Promise<TextToPlaceResponse> {
-  try {
-    // Use Google Autocomplete API to get place_id, lat, and lng from the address
-    const requestString = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${import.meta.env.VITE_GOOGLE_MAP_KEY_SERVER}`
-    const response = await fetch(requestString);
-    const {results} = await response.json();
-    // Return the place_id and lat/lng of the first result
-    return {
-      place_id: results[0].place_id,
-      lat: results[0].geometry.location.lat,
-      lng: results[0].geometry.location.lng,
-    }
-  }
-  catch (error) {
-    console.error(error);
-    return {
-      place_id: null,
-      lat: null,
-      lng: null,
-    }
-  }
+async function textAddressToPlace(
+  address: string
+): Promise<TextToPlaceResponse> {
+  // Use Google Autocomplete API to get place_id, lat, and lng from the address
+  const { data, error } = await geocodeAddress(address);
+  if (error) return { place_id: null, lat: null, lng: null };
+  const { results } = data;
+  // Return the place_id and lat/lng of the first result
+  return {
+    place_id: results[0].place_id,
+    lat: results[0].geometry.location.lat,
+    lng: results[0].geometry.location.lng
+  };
 }
-
 
 export async function get() {
-  const res = await textAddressToPlace("Boston, MA, USA");
-  return {status: 200, body: res};
+  const textToPlaceResponse = await textAddressToPlace('Boston, MA, USA');
+  return {
+    status: 200,
+    body: { textToPlaceResponse, data: await getPlaceIdFromFacebook() }
+  };
 }
 
-
-
-
-
-
 export async function uploadPlaceToSupabase(payload: Payload) {
-  const {error: errorPlaces} = await supabase.from('places').upsert(payload, {
+  const { error: errorPlaces } = await supabase.from('places').upsert(payload, {
     returning: 'minimal' // Don't return the value after inserting
   });
   if (errorPlaces) throw errorPlaces;
@@ -58,8 +63,8 @@ export async function uploadUserPlaceSelectionToSupabase(
   place_id: string
 ) {
   if (!user_response_id) return;
-  const {error: errorUserIDtoPlaceID} = await supabase
+  const { error: errorUserIDtoPlaceID } = await supabase
     .from('user_responses')
-    .upsert({place_id, user_response_id}, {returning: 'minimal'});
+    .upsert({ place_id, user_response_id }, { returning: 'minimal' });
   if (errorUserIDtoPlaceID) throw errorUserIDtoPlaceID;
 }
