e9dea3e06b3f958fc2fb76fa4f660ef8bdec1e9a
Braden Wong <bmw02002turbo@gmail.com>
2022-05-28 22:31:47 -0700

    Feat: List Interests

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -4,14 +4,14 @@
 
   export type UserProfileInformation = definitions['users'] & {
     user_responses: {
+      interests: string;
+      expression: string;
+      university: string;
+      instagram: string;
+      linkedin: string;
+      phone: string;
+      major: string;
       places: {
-        interests: string;
-        expression: string;
-        university: string;
-        instagram: string;
-        linkedin: string;
-        phone: string;
-        major: string;
         description: string;
       };
     };
@@ -36,11 +36,58 @@
   import UserCard from './CardOfUser.svelte';
 
   export let userProfileInformation: UserProfileInformation;
+  const userResponses = [
+    {
+      name: 'interests',
+      label: 'Interests',
+      icon: 'heart',
+      value: userProfileInformation.user_responses.interests
+    },
+    {
+      name: 'expression',
+      label: 'Expression',
+      icon: 'smile',
+      value: userProfileInformation.user_responses.expression
+    },
+    {
+      name: 'university',
+      label: 'University',
+      icon: 'graduation-cap',
+      value: userProfileInformation.user_responses.university
+    },
+    {
+      name: 'instagram',
+      label: 'Instagram',
+      icon: 'instagram',
+      value: userProfileInformation.user_responses.instagram
+    },
+    {
+      name: 'linkedin',
+      label: 'Linkedin',
+      icon: 'linkedin',
+      value: userProfileInformation.user_responses.linkedin
+    },
+    {
+      name: 'phone',
+      label: 'Phone',
+      icon: 'phone',
+      value: userProfileInformation.user_responses.phone
+    },
+    {
+      name: 'major',
+      label: 'Major',
+      icon: 'book',
+      value: userProfileInformation.user_responses.major
+    }
+  ];
 </script>
 
 <svelte:head>
   <title>{userProfileInformation.name}</title>
   <meta name="description" content="Profile information" />
+  <script
+    src="https://kit.fontawesome.com/f538ad6371.js"
+    crossorigin="anonymous"></script>
 </svelte:head>
 
 <div class="hero min-h-screen-nav bg-base-200">
@@ -52,7 +99,23 @@
       <div
         class="grid h-100 flex-grow card shadow-2xl bg-base-100 rounded-box place-items-center"
       >
-        <div class="card-body">content</div>
+        {#each userResponses as { name, label, icon, value }}
+          <div class="form-control">
+            <label class="label" for="location">
+              <span class="label-text">{label}</span>
+              <i class="fas fa-{icon} text-base" />
+            </label>
+            <input
+              tabindex="0"
+              type="text"
+              id="location"
+              class="input input-bordered"
+              disabled
+              placeholder="Start typing your city here..."
+              bind:value
+            />
+          </div>
+        {/each}
       </div>
     </div>
 
