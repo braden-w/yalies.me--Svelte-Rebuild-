bcfda6c2d01ba83a2666a56c82325cb58bf07143
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 03:09:16 -0700

    Feat: Loop over response types

--- src/routes/users/UserResponsesCard.svelte
+++ src/routes/users/UserResponsesCard.svelte
@@ -2,6 +2,50 @@
   import type { UserProfileInformation } from 'src/routes/users/[id].svelte';
 
   export let userProfileInformation: UserProfileInformation;
+  const userResponses = [
+    {
+      name: 'interests',
+      label: 'Interests',
+      icon: 'heart',
+      value: userProfileInformation.user_responses.interests
+    },
+    {
+      name: 'expression',
+      label: 'Spotify',
+      icon: 'spotify',
+      value: userProfileInformation.user_responses.expression
+    },
+    {
+      name: 'university',
+      label: 'University',
+      icon: 'graduation-cap',
+      value: userProfileInformation.user_responses.university
+    },
+    {
+      name: 'instagram',
+      label: 'Instagram',
+      icon: 'instagram',
+      value: userProfileInformation.user_responses.instagram
+    },
+    {
+      name: 'linkedin',
+      label: 'Linkedin',
+      icon: 'linkedin',
+      value: userProfileInformation.user_responses.linkedin
+    },
+    {
+      name: 'phone',
+      label: 'Phone',
+      icon: 'phone',
+      value: userProfileInformation.user_responses.phone
+    },
+    {
+      name: 'major',
+      label: 'Major',
+      icon: 'book',
+      value: userProfileInformation.user_responses.major
+    }
+  ];
 </script>
 
 <div
@@ -13,18 +57,26 @@
       A manner of expressing yourself
     </div>
 
-    <div class="form-control">
-      <label class="label cursor-pointer"
-        ><span class="label-text">Enable teleportation</span>
-        <input type="checkbox" class="toggle toggle-primary" /></label
-      >
-    </div>
-    <div class="form-control">
-      <label class="label cursor-pointer"
-        ><span class="label-text">Enable time travel</span>
-        <input type="checkbox" class="toggle toggle-secondary" /></label
-      >
-    </div>
+    {#each userResponses as { name, label, icon, value }}
+      <div class="form-control">
+        <label class="label" for={name}>
+          <span class="label-text">{label}</span>
+          <button class="btn btn-xs btn-primary btn-circle">
+            <i class="fa fa-{icon}" /></button
+          >
+        </label>
+        <input
+          tabindex="0"
+          type="text"
+          id={name}
+          class="input input-bordered"
+          disabled
+          placeholder="Start typing your response here..."
+          bind:value
+        />
+      </div>
+    {/each}
+
     <div class="form-control">
       <label class="label cursor-pointer"
         ><span class="label-text">Enable laser eyes</span>
