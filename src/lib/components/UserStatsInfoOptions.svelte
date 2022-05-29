a3433b8b6dfda317d94fb8d3bb2c20057baf9e6a
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 02:46:29 -0700

    Chore: Remove dropdown

--- src/lib/components/UserStatsInfoOptions.svelte
+++ src/lib/components/UserStatsInfoOptions.svelte
@@ -8,47 +8,27 @@
 <div
   class="col-span-3 row-span-3 mx-2 flex w-72 flex-shrink-0 flex-col xl:mx-0 xl:w-full"
 >
-  <div class="dropdown">
-    <div tabindex="0" class="bg-opacity-100">
-      <div class="tabs w-full flex-grow-0">
-        <button
-          class="tab tab-lifted tab-border-none tab-lg flex-1"
-          class:tab-active={tab === 0}
-        >
-          Stats
-        </button>
-        <button
-          class="tab tab-lifted tab-border-none tab-lg flex-1"
-          class:tab-active={tab === 1}
-        >
-          Info
-        </button>
-        <button
-          class="tab tab-lifted tab-border-none tab-lg flex-1"
-          class:tab-active={tab === 2}
-        >
-          Options
-        </button>
-      </div>
-    </div>
-    <div tabindex="0" class="dropdown-content py-2">
-      <div
-        class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl"
-      >
-        <div class="card-body">
-          <h2 class="card-title font-extrabold capitalize">tab component</h2>
-          <p class="text-neutral-content text-sm text-opacity-80">
-            Beautiful tabs to switch between sections
-          </p>
-          <div class="mt-4 flex justify-end">
-            <a href="/components/tab" class="btn btn-primary btn-sm xl:btn-md"
-              >See component</a
-            >
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="tabs w-full flex-grow-0">
+    <button
+      class="tab tab-lifted tab-border-none tab-lg flex-1"
+      class:tab-active={tab === 0}
+    >
+      Stats
+    </button>
+    <button
+      class="tab tab-lifted tab-border-none tab-lg flex-1"
+      class:tab-active={tab === 1}
+    >
+      Info
+    </button>
+    <button
+      class="tab tab-lifted tab-border-none tab-lg flex-1"
+      class:tab-active={tab === 2}
+    >
+      Options
+    </button>
   </div>
+
   <div
     class="bg-base-100 grid w-full flex-grow gap-3 rounded-xl rounded-tl-none p-6 shadow-xl"
   >
