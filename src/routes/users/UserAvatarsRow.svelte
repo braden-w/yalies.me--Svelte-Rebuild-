ff3434d720c87c6be63d38f1262f81503fe15fe0
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 14:47:03 -0700

    Fix: UserAvatarsRow

--- src/routes/users/UserAvatarsRow.svelte
+++ src/routes/users/UserAvatarsRow.svelte
@@ -1,54 +1,48 @@
-
-
 <script lang="ts">
-import type { UserProfileInformation } from "src/routes/users/[id].svelte";
-
+  import type { UserProfileInformation } from 'src/routes/users/[id].svelte';
 
   export let userProfileInformation: UserProfileInformation;
 </script>
 
-<div class="col-span-1 row-span-1">
-        <div
-          class="mx-2 flex w-72 flex-shrink-0 flex-col justify-center xl:mx-0 xl:w-full"
-        >
-          <div class="flex items-center justify-between">
-            <div class="online avatar">
-              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                <img
-                  src={userProfileInformation.avatar_url}
-                  alt="Avatar Tailwind CSS Component"
-                  class="mask mask-squircle"
-                />
-              </div>
-            </div>
-            <div class="online avatar">
-              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                <img
-                  src={userProfileInformation.avatar_url}
-                  alt="Avatar Tailwind CSS Component"
-                  class="mask mask-squircle"
-                />
-              </div>
-            </div>
-            <div class="avatar offline">
-              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                <img
-                  src={userProfileInformation.avatar_url}
-                  alt="Avatar Tailwind CSS Component"
-                  class="mask mask-squircle"
-                />
-              </div>
-            </div>
-            <div class="avatar">
-              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                <img
-                  src={userProfileInformation.avatar_url}
-                  alt="Avatar Tailwind CSS Component"
-                  class="mask mask-squircle"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+<div
+  class="mx-2 flex w-72 flex-shrink-0 flex-col justify-center xl:mx-0 xl:w-full"
+>
+  <div class="flex items-center justify-between">
+    <div class="online avatar">
+      <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+        <img
+          src={userProfileInformation.avatar_url}
+          alt="Avatar Tailwind CSS Component"
+          class="mask mask-squircle"
+        />
       </div>
-
+    </div>
+    <div class="online avatar">
+      <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+        <img
+          src={userProfileInformation.avatar_url}
+          alt="Avatar Tailwind CSS Component"
+          class="mask mask-squircle"
+        />
+      </div>
+    </div>
+    <div class="avatar offline">
+      <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+        <img
+          src={userProfileInformation.avatar_url}
+          alt="Avatar Tailwind CSS Component"
+          class="mask mask-squircle"
+        />
+      </div>
+    </div>
+    <div class="avatar">
+      <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+        <img
+          src={userProfileInformation.avatar_url}
+          alt="Avatar Tailwind CSS Component"
+          class="mask mask-squircle"
+        />
+      </div>
+    </div>
+  </div>
+</div>
