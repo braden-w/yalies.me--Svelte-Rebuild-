ed375499a2a36528ae70b60d60e1592f98ca97b8
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 03:01:15 -0700

    Fix: Pass userProfileInformation prop

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -33,6 +33,8 @@
 </script>
 
 <script lang="ts">
+  import UserResponsesCard from '../../lib/components/UserResponses.svelte';
+
   import UserLocationCard from '../../lib/components/UserLocationCard.svelte';
 
   import UserStatsInfoOptions from '../../lib/components/UserStatsInfoOptions.svelte';
@@ -568,97 +570,7 @@
       </div>
       <UserLocationCard {userProfileInformation} />
 
-      <div
-        class="col-span-3 row-span-2 mx-2 grid w-72 flex-shrink-0 gap-4 xl:mx-0 xl:w-auto xl:place-self-stretch"
-      >
-        <div class="bg-base-100 text-base-content rounded-box shadow-xl">
-          <div class="dropdown dropdown-end w-full">
-            <div tabindex="0">
-              <ul class="menu overflow-visible p-3">
-                <li class="menu-title"><span>Menu Title</span></li>
-                <li>
-                  <button
-                    ><svg
-                      width="20"
-                      height="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      class="mr-2 inline-block h-5 w-5 stroke-current"
-                      ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-                      /></svg
-                    >
-                    Menu Item 1</button
-                  >
-                </li>
-                <li>
-                  <button
-                    ><svg
-                      width="20"
-                      height="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      class="mr-2 inline-block h-5 w-5 stroke-current"
-                      ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
-                      /></svg
-                    >
-                    Menu Item 2</button
-                  >
-                </li>
-                <li>
-                  <button
-                    ><svg
-                      width="20"
-                      height="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      class="mr-2 inline-block h-5 w-5 stroke-current"
-                      ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                      /></svg
-                    >
-                    Menu Item 3
-                    <div class="badge badge-success">new</div></button
-                  >
-                </li>
-              </ul>
-            </div>
-            <div tabindex="0" class="dropdown-content py-2">
-              <div
-                class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl"
-              >
-                <div class="card-body">
-                  <h2 class="card-title font-extrabold capitalize">
-                    menu component
-                  </h2>
-                  <p class="text-neutral-content text-sm text-opacity-80">
-                    Use it for sidebar or any other list of items
-                  </p>
-                  <div class="mt-4 flex justify-end">
-                    <a
-                      href="/components/menu"
-                      class="btn btn-primary btn-sm xl:btn-md">See component</a
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <UserResponsesCard {userProfileInformation} />
       <div
         class="col-span-3 row-span-1 mx-2 grid w-72 flex-shrink-0 gap-4 xl:mx-0 xl:w-auto"
       >
