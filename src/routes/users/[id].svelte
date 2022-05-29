45d703ed043a0a6e7776d3dd65406563092b6690
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 11:06:36 -0700

    Feat: Stretch items on large mode

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -67,7 +67,7 @@
   <div class="w-full h-full">
     <div
       id="component-demo"
-      class="flex w-full items-center gap-4 overflow-y-hidden overflow-x-scroll px-10 pt-1 pb-10 xl:grid grid-flow-col grid-rows-6 xl:overflow-x-auto xl:px-4"
+      class="flex w-full items-center gap-4 overflow-y-hidden overflow-x-scroll px-10 pt-1 pb-10 xl:grid xl:items-stretch grid-flow-col grid-rows-6 xl:overflow-x-auto xl:px-4"
     >
       <!-- Cell 1 -->
       <div class="bg-teal-400 row-span-2">
