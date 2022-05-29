bae350e33abca4b81ef7fe8892af4e7462de6c1e
Braden Wong <bmw02002turbo@gmail.com>
2022-05-28 21:35:49 -0700

    Scaffold Layout

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -45,7 +45,53 @@
 
 <div class="hero min-h-screen-nav bg-base-200">
   <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="flex w-full">
+      <div
+        class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+      >
+        content
+      </div>
+      <div class="divider divider-horizontal">OR</div>
+      <div
+        class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+      >
+        content
+      </div>
+    </div>
     <UserCard {userProfileInformation} />
     <!-- Create a card that displays -->
+    <!-- the user's information -->
+    <div class="flex-1">
+      <div class="flex flex-col">
+        <div class="flex-1">
+          <h2 class="text-2xl font-bold">
+            {userProfileInformation.name}
+          </h2>
+        </div>
+        <div class="flex-1">
+          <p class="text-base">
+            {userProfileInformation.user_responses.interests}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.expression}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.university}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.instagram}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.linkedin}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.phone}
+          </p>
+          <p class="text-base">
+            {userProfileInformation.user_responses.major}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
