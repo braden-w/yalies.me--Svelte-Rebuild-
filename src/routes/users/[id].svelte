e8a1759d2c184d1cf6380ed85aa3adacf8ccd3c0
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 10:42:55 -0700

    Fixing Up Grid Layout

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -96,18 +96,14 @@
 
 <div class="w-full h-full">
   <div class="grid grid-flow-col grid-rows-6">
-    <div class="bg-teal-400 row-span-3">1</div>
-    <div class="bg-teal-400 row-span-3">2</div>
+    <div class="bg-teal-400 row-span-2">1</div>
+    <div class="bg-teal-400 row-span-1">2</div>
     <div class="bg-teal-400 row-span-3">3</div>
-    <div class="bg-teal-400 row-span-3">4</div>
-    <div class="bg-teal-400 row-span-3">5</div>
+    <div class="bg-teal-400 row-span-6">4</div>
     <div class="bg-teal-400 row-span-3">6</div>
-    <div class="bg-teal-400 row-span-3">7</div>
-    <div class="bg-teal-400 row-span-3">8</div>
-    <div class="bg-teal-400 row-span-3">9</div>
-    <div class="bg-teal-400 row-span-3">10</div>
-    <div class="bg-teal-400 row-span-3">11</div>
-    <div class="bg-teal-400 row-span-3">12</div>
+    <div class="bg-teal-400 row-span-3">6</div>
+    <div class="bg-teal-400 row-span-4">7</div>
+    <div class="bg-teal-400 row-span-2">7</div>
   </div>
 </div>
 
