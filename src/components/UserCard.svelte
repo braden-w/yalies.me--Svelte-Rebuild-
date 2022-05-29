223baa39426277cce63bca3721d547383cb5a341
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 15:18:52 -0700

    Feat: Icons

--- src/components/UserCard.svelte
+++ src/components/UserCard.svelte
@@ -47,29 +47,57 @@
           class="badge badge-primary"
           href={`mailto:${userProfileInformation.email}`}
         >
-          Email
-        </a>
+          <!-- Make an svg email icon -->
+          <svg
+            class="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg></a
+        >
       {/if}
       {#if userResponses.phone}
         <a class="badge badge-primary" href={`sms:${userResponses.phone}`}>
-          Text
+          <!-- Make a svg phone icon -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+            />
+          </svg>
         </a>
       {/if}
 
       {#if userResponses.expression}
         <a
-          class="badge badge-primary"
+          class="badge badge-accent"
           target="_blank"
           href={userResponses.expression}
         >
-          Spotify
+          <!-- Make a Green Spotify Icon -->
         </a>
       {/if}
       {#if userResponses.instagram}
         <a
           class="badge badge-primary"
           target="_blank"
-          href={`https://www.instagram.com/${currentSelectedUser.instagram}/`}
+          href={`https://www.instagram.com/${userResponses.instagram}/`}
         >
           Instagram
         </a>
