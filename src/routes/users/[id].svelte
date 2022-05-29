2f9d8bc8f0a4ce1d0f451153b48332b0addad761
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 13:59:05 -0700

    Chore: Remove Dialog

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -66,65 +66,41 @@
         <div
           class="row-span-1 mx-2 flex w-72 flex-shrink-0 flex-col justify-center xl:mx-0 xl:w-full"
         >
-          <div class="dropdown-end dropdown w-full">
-            <div tabindex="0">
-              <div class="flex items-center justify-between">
-                <div class="online avatar">
-                  <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                    <img
-                      src={userProfileInformation.avatar_url}
-                      alt="Avatar Tailwind CSS Component"
-                      class="mask mask-squircle"
-                    />
-                  </div>
-                </div>
-                <div class="online avatar">
-                  <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                    <img
-                      src={userProfileInformation.avatar_url}
-                      alt="Avatar Tailwind CSS Component"
-                      class="mask mask-squircle"
-                    />
-                  </div>
-                </div>
-                <div class="avatar offline">
-                  <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                    <img
-                      src={userProfileInformation.avatar_url}
-                      alt="Avatar Tailwind CSS Component"
-                      class="mask mask-squircle"
-                    />
-                  </div>
-                </div>
-                <div class="avatar">
-                  <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
-                    <img
-                      src={userProfileInformation.avatar_url}
-                      alt="Avatar Tailwind CSS Component"
-                      class="mask mask-squircle"
-                    />
-                  </div>
-                </div>
+          <div class="flex items-center justify-between">
+            <div class="online avatar">
+              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+                <img
+                  src={userProfileInformation.avatar_url}
+                  alt="Avatar Tailwind CSS Component"
+                  class="mask mask-squircle"
+                />
               </div>
             </div>
-            <div tabindex="0" class="dropdown-content py-2">
-              <div
-                class="compact card rounded-box w-72 bg-neutral-focus text-neutral-content shadow-xl"
-              >
-                <div class="card-body">
-                  <h2 class="card-title font-extrabold capitalize">
-                    avatar component
-                  </h2>
-                  <p class="text-sm text-neutral-content text-opacity-80">
-                    Use avatar component with any size
-                  </p>
-                  <div class="mt-4 flex justify-end">
-                    <a
-                      href="/components/avatar"
-                      class="btn btn-primary btn-sm xl:btn-md">See component</a
-                    >
-                  </div>
-                </div>
+            <div class="online avatar">
+              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+                <img
+                  src={userProfileInformation.avatar_url}
+                  alt="Avatar Tailwind CSS Component"
+                  class="mask mask-squircle"
+                />
+              </div>
+            </div>
+            <div class="avatar offline">
+              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+                <img
+                  src={userProfileInformation.avatar_url}
+                  alt="Avatar Tailwind CSS Component"
+                  class="mask mask-squircle"
+                />
+              </div>
+            </div>
+            <div class="avatar">
+              <div class="mask mask-squircle h-16 w-16 bg-base-100 p-1">
+                <img
+                  src={userProfileInformation.avatar_url}
+                  alt="Avatar Tailwind CSS Component"
+                  class="mask mask-squircle"
+                />
               </div>
             </div>
           </div>
@@ -136,13 +112,10 @@
       </div>
       <!-- Cell 4 -->
       <div class="row-span-6 bg-teal-400">
-        <UserResponsesCard
-          {userProfileInformation}
-        />
+        <UserResponsesCard {userProfileInformation} />
       </div>
       <!-- Cell 5 -->
-      <div class="row-span-3 bg-teal-400">
-      </div>
+      <div class="row-span-3 bg-teal-400" />
       <!-- Cell 6 -->
       <div class="row-span-3 bg-teal-400">
         <SpotifyPlayer url={userProfileInformation.user_responses.expression} />
