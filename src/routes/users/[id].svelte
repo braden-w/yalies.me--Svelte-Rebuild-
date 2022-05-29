d99a994379cec268635b38ed474f6d1575e747a9
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 11:24:03 -0700

    Feat: Flex Col

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -54,7 +54,7 @@
   <div class="w-full h-full">
     <div
       id="component-demo"
-      class="w-full h-full grid grid-flow-col grid-rows-6 items-stretch overflow-x-auto gap-4 px-4 pt-1 pb-10"
+      class="w-full h-full flex flex-col xl:grid xl:grid-flow-col xl:grid-cols-4 xl:grid-rows-6 items-stretch overflow-x-auto gap-4 px-4 pt-1 pb-10"
     >
       <!-- Cell 1 -->
       <div class="bg-teal-400 row-span-2">
