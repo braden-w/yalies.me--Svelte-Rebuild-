e92b079ec5c67bbf04a8422f8c0bf193d6ae7ad8
Braden Wong <bmw02002turbo@gmail.com>
2022-05-28 21:30:00 -0700

    Types

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -1,12 +1,20 @@
 <script context="module" lang="ts">
+  import type { definitions } from '$lib/types/supabase';
   import { supabase } from '$lib/utils/supabaseClient';
+
+  export type UserProfileInformation = definitions['users'] & {
+    user_responses: {
+      places: {
+        description: string;
+      };
+    };
+  };
   export async function load({ params }: { params: { id: string } }) {
     const { data, error } = await supabase
-      .from('users')
+      .from<definitions['users']>('users')
       .select('name, avatar_url, user_responses(places(description))')
       .eq('id', params.id)
       .maybeSingle();
-
     if (error) return { status: error.code, props: { error } };
     return {
       status: 200,
@@ -18,16 +26,7 @@
 <script lang="ts">
   import UserCard from './CardOfUser.svelte';
 
-  export let userProfileInformation: {
-    id: string;
-    name: string;
-    avatar_url: string;
-    user_responses: {
-      places: {
-        description: string;
-      };
-    };
-  };
+  export let userProfileInformation: UserProfileInformation;
 </script>
 
 <svelte:head>
@@ -37,7 +36,7 @@
 
 <div class="hero min-h-screen-nav bg-base-200">
   <div class="hero-content flex-col lg:flex-row-reverse">
-    <UserCard {userProfileInformation}></UserCard>
+    <UserCard {userProfileInformation} />
     <!-- Create a card that displays -->
   </div>
 </div>
