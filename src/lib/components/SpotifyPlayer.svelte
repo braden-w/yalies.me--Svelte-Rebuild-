857a8a82f7ab86efe8c1ab9eed02eb0b1b4e0ab0
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 00:03:38 -0700

    SpotifyPlayer: create

---
+++ src/lib/components/SpotifyPlayer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let url: string;
+  const defaultPlaylistID = '1uUHti2X6KkpUFcOr4FJ8v';
+  $: playlistID = getPlaylistID();
+  function getPlaylistID() {
+    if (/^(spotify:|https:\/\/[a-z]+\.spotify\.com\/)/.test(url) === false) {
+      return defaultPlaylistID;
+    }
+    return (
+      url.match(
+        /^(https:\/\/open.spotify.com\/playlist\/|spotify:user:spotify:playlist:)([a-zA-Z0-9]+)(.*)$/
+      )?.[2] ?? defaultPlaylistID
+    );
+  }
+</script>
+
+<iframe
+  title="Spotify Player"
+  src={`https://open.spotify.com/embed/playlist/${playlistID}`}
+  width="100%"
+  height="100%"
+  frameborder="0"
+  allowtransparency={true}
+  allow="encrypted-media"
+/>
