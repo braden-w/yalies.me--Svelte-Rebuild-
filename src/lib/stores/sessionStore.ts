a4f9d046ff0e36b0d39268b654f8fde580436fdf
Braden Wong <bmw02002turbo@gmail.com>
2022-05-30 08:18:18 -0700

    Feat: SessionStore Writable to Localstorage

--- src/lib/stores/sessionStore.ts
+++ src/lib/stores/sessionStore.ts
@@ -2,6 +2,7 @@ import { writable, type Writable, get } from 'svelte/store';
 import type { definitions } from '$lib/types/supabase';
 import { supabase } from '$lib/utils/supabaseClient';
 
+
 export async function refreshSessionStore() {
   const { data, error } = await supabase
     .from<definitions['users_facebook_places']>('users_facebook_places')
@@ -22,6 +23,8 @@ export async function refreshSessionStore() {
   }
   return null;
 }
+
 export const sessionStore: Writable<
   definitions['users_facebook_places'] | null
-> = writable(null);
+> = writable( localStorage.stored_object ? JSON.parse(localStorage.stored_object) : {});
+sessionStore.subscribe(val => localStorage.setItem("yalies.me-sessionStore",JSON.stringify(val)));
