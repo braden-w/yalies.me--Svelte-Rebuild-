ac5ede1f65a098f0291b433ad4c17e281467f270
Braden Wong <bmw02002turbo@gmail.com>
2022-05-29 20:10:15 -0700

    Feat/Fix: Await Loading Session Before Populating Data

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -8,13 +8,10 @@
   import type { definitions } from '$lib/types/supabase';
   import { refreshSessionStore, sessionStore } from '$lib/stores/sessionStore';
 
-  export let userProfileInformation:
-    | definitions['users_facebook_places']
-    | null;
-  async function prepopulateUserProfileInformation() {
-    return (userProfileInformation = await refreshSessionStore());
-  }
-  prepopulateUserProfileInformation();
+  export let userProfileInformation: Promise<
+    definitions['users_facebook_places'] | null
+  >;
+  userProfileInformation = refreshSessionStore();
 </script>
 
 <svelte:head>
@@ -28,63 +25,65 @@
 
 <div class="min-h-screen-nav hero bg-base-200">
   <div class="h-full w-full">
-    <div
-      id="component-demo"
-      class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6"
-    >
-      <!-- Cell 1 -->
-      <div class="col-span-1 row-span-2">
-        <UserCard
-          name={$sessionStore?.name}
-          avatar_url={$sessionStore?.avatar_url}
-        />
-      </div>
-      <div class="col-span-1 row-span-1">
-        <UserSocials
-          email={$sessionStore?.email}
-          phone={$sessionStore?.phone}
-          expression={$sessionStore?.expression}
-          instagram={$sessionStore?.instagram}
-          linkedin={$sessionStore?.linkedin}
-        />
-      </div>
-      <!-- Cell 2 -->
-      <div class="col-span-1 row-span-3">
-        <UserLocationCard
-          id={$sessionStore?.id}
-          description={$sessionStore?.description}
-        />
-        <!-- <UserAvatarsRow {userProfileInformation} /> -->
-      </div>
-      <!-- Cell 3 -->
-      <div class="col-span-1 row-span-6">
-        <SpotifyPlayer url={$sessionStore?.expression} />
-      </div>
-      <!-- Cell 4 -->
-      <div class="col-span-3 row-span-6">
-        <UserResponsesCard
-          id={$sessionStore?.id}
-          interests={$sessionStore?.interests}
-          major={$sessionStore?.major}
-          phone={$sessionStore?.phone}
-          expression={$sessionStore?.expression}
-          instagram={$sessionStore?.instagram}
-          linkedin={$sessionStore?.linkedin}
-        />
-      </div>
-      <!-- Cell 5 -->
-      <!-- <div class="col-span-2 row-span-3"> -->
-      <!-- <UserStatsInfoOptions {userProfileInformation} /> -->
-      <!-- </div> -->
-      <!-- Cell 6 -->
-      <!-- <div class="col-span-2 row-span-3"> -->
-      <!-- </div> -->
+    {#await userProfileInformation then userProfileInformation}
+      <div
+        id="component-demo"
+        class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6"
+      >
+        <!-- Cell 1 -->
+        <div class="col-span-1 row-span-2">
+          <UserCard
+            name={userProfileInformation?.name}
+            avatar_url={userProfileInformation?.avatar_url}
+          />
+        </div>
+        <div class="col-span-1 row-span-1">
+          <UserSocials
+            email={userProfileInformation?.email}
+            phone={userProfileInformation?.phone}
+            expression={userProfileInformation?.expression}
+            instagram={userProfileInformation?.instagram}
+            linkedin={userProfileInformation?.linkedin}
+          />
+        </div>
+        <!-- Cell 2 -->
+        <div class="col-span-1 row-span-3">
+          <UserLocationCard
+            id={userProfileInformation?.id}
+            description={userProfileInformation?.description}
+          />
+          <!-- <UserAvatarsRow {userProfileInformation} /> -->
+        </div>
+        <!-- Cell 3 -->
+        <div class="col-span-1 row-span-6">
+          <SpotifyPlayer url={userProfileInformation?.expression} />
+        </div>
+        <!-- Cell 4 -->
+        <div class="col-span-3 row-span-6">
+          <UserResponsesCard
+            id={userProfileInformation?.id}
+            interests={userProfileInformation?.interests}
+            major={userProfileInformation?.major}
+            phone={userProfileInformation?.phone}
+            expression={userProfileInformation?.expression}
+            instagram={userProfileInformation?.instagram}
+            linkedin={userProfileInformation?.linkedin}
+          />
+        </div>
+        <!-- Cell 5 -->
+        <!-- <div class="col-span-2 row-span-3"> -->
+        <!-- <UserStatsInfoOptions {userProfileInformation} /> -->
+        <!-- </div> -->
+        <!-- Cell 6 -->
+        <!-- <div class="col-span-2 row-span-3"> -->
+        <!-- </div> -->
 
-      <!-- 
+        <!-- 
       
       <UserMenuItems {userProfileInformation} />
        -->
-    </div>
+      </div>
+    {/await}
   </div>
 </div>
 <!-- <div class="divider divider-horizontal">OR</div> -->
