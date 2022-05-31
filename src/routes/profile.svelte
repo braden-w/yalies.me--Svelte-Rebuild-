0beb3149972f430db14766c99bab8d6396f565cc
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 10:19:59 -0700

    Feat:Responsive Layout

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -18,30 +18,30 @@
   <div class="h-full w-full">
     <div
       id="component-demo"
-      class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6"
+      class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 md:grid md:grid-flow-col md:grid-cols-12 md:grid-rows-6"
     >
       <!-- Cell 1 -->
-      <div class="col-span-1 row-span-3">
+      <div class="col-span-4 row-span-3">
         <UserCard bind:userProfileInformation={$profileStore} />
         <div class="rounded-box mt-4 bg-neutral py-2 shadow-xl xl:w-full">
           <UserSocials bind:userProfileInformation={$profileStore} />
         </div>
       </div>
       <!-- Cell 2 -->
-      <div class="col-span-1 row-span-3">
+      <div class="col-span-4 row-span-3">
         <SpotifyPlayer url={$profileStore?.expression} />
         <!-- <UserAvatarsRow {userProfileInformation} /> -->
       </div>
       <!-- Cell 3 -->
-      <div class="col-span-1 row-span-3">
+      <div class="col-span-4 row-span-3 lg:col-span-3">
         <UserLocationCard bind:userProfileInformation={$profileStore} />
       </div>
       <!-- Cell 4 -->
-      <div class="col-span-1 row-span-3">
+      <div class="col-span-4 row-span-3 lg:col-span-3">
         <ListOfLocations />
       </div>
       <!-- Cell 5 -->
-      <div class="col-span-3 row-span-6">
+      <div class="col-span-4 row-span-6 lg:col-span-6">
         <UserResponsesCard bind:userProfileInformation={$profileStore} />
       </div>
       <!-- <div class="col-span-2 row-span-3"> -->
