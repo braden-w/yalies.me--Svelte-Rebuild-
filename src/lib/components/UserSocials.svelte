74c75052e6664cc7ed3eabf1a4350d3a174dcbb7
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 02:48:47 -0700

    Feat: UserSocials Move into Box

--- src/lib/components/UserSocials.svelte
+++ src/lib/components/UserSocials.svelte
@@ -12,84 +12,80 @@
     | null;
 </script>
 
-<div
-  class="rounded-box w-72 place-items-center items-center gap-4 bg-base-100 p-4 py-8 shadow-xl xl:mx-0 xl:w-full"
->
-  <div class="mt-2 text-center">
-    {#if userProfileInformation?.email}
-      <a
-        class="btn btn-circle btn-sm"
-        href={`mailto:${userProfileInformation?.email}`}
+<div class="mt-2 text-center">
+  {#if userProfileInformation?.email}
+    <a
+      class="btn btn-circle btn-sm"
+      href={`mailto:${userProfileInformation?.email}`}
+    >
+      <!-- Make an svg email icon -->
+      <svg
+        class="h-6 w-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <!-- Make an svg email icon -->
-        <svg
-          class="h-6 w-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-          />
-        </svg>
-      </a>
-    {/if}
-    {#if userProfileInformation?.phone}
-      <a
-        class="btn btn-circle btn-sm"
-        href={`sms:${userProfileInformation?.phone}`}
-      >
-        <!-- Make a svg phone icon -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
           stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-          />
-        </svg>
-      </a>
-    {/if}
+          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />
+      </svg>
+    </a>
+  {/if}
+  {#if userProfileInformation?.phone}
+    <a
+      class="btn btn-circle btn-sm"
+      href={`sms:${userProfileInformation?.phone}`}
+    >
+      <!-- Make a svg phone icon -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+        />
+      </svg>
+    </a>
+  {/if}
 
-    {#if userProfileInformation?.expression}
-      <a
-        class="btn btn-circle btn-sm"
-        target="_blank"
-        href={userProfileInformation?.expression}
-      >
-        <!-- Make a Green Spotify Icon -->
-        <SpotifyIcon />
-      </a>
-    {/if}
-    {#if userProfileInformation?.instagram}
-      <a
-        class="btn btn-circle btn-sm"
-        target="_blank"
-        href={`https://www.instagram.com/${userProfileInformation?.instagram}/`}
-      >
-        <!-- Make an Instagram SVG Icon -->
-        <InstagramIcon />
-      </a>
-    {/if}
-    {#if userProfileInformation?.linkedin}
-      <a
-        class="btn btn-circle btn-sm"
-        target="_blank"
-        href={userProfileInformation?.linkedin}
-      >
-        <!-- Make an LinkedIn SVG Icon -->
-        <LinkedInIcon />
-      </a>
-    {/if}
-  </div>
+  {#if userProfileInformation?.expression}
+    <a
+      class="btn btn-circle btn-sm"
+      target="_blank"
+      href={userProfileInformation?.expression}
+    >
+      <!-- Make a Green Spotify Icon -->
+      <SpotifyIcon />
+    </a>
+  {/if}
+  {#if userProfileInformation?.instagram}
+    <a
+      class="btn btn-circle btn-sm"
+      target="_blank"
+      href={`https://www.instagram.com/${userProfileInformation?.instagram}/`}
+    >
+      <!-- Make an Instagram SVG Icon -->
+      <InstagramIcon />
+    </a>
+  {/if}
+  {#if userProfileInformation?.linkedin}
+    <a
+      class="btn btn-circle btn-sm"
+      target="_blank"
+      href={userProfileInformation?.linkedin}
+    >
+      <!-- Make an LinkedIn SVG Icon -->
+      <LinkedInIcon />
+    </a>
+  {/if}
 </div>
