4b881abd48390cfbf396640f680aece498d41706
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 11:00:47 -0700

    Feat: Try Wrapping List in List

--- src/lib/components/ListOfLocations.svelte
+++ src/lib/components/ListOfLocations.svelte
@@ -17,35 +17,39 @@
 </script>
 
 <div
-  class="rounded-box max-h-96 overflow-y-auto bg-base-100 text-base-content shadow-xl"
+  class="rounded-box mx-2 flex flex-shrink-0 flex-col justify-center gap-4 bg-base-100 p-4 shadow-xl xl:mx-0 xl:w-full"
 >
-  <ul class="menu overflow-visible p-3">
-    <li class="menu-title">
-      <span>Locations</span>
-    </li>
-    {#await promise}
-      Loading...
-    {:then { data: places }}
-      {#if places}
-        {#each places as place}
-          <li>
-            <a href={`/places/${place.place_id}`}>
-              {@html generateStackOfIcons({
-                threeAvatars: getThreeAvatarUrls(place) ?? [],
-                indicator: (place.people ?? []).length
-              })}
+  <div
+    class="rounded-box max-h-96 overflow-y-auto bg-neutral text-base-content shadow-xl"
+  >
+    <ul class="menu overflow-visible p-3">
+      <li class="menu-title">
+        <span>Locations</span>
+      </li>
+      {#await promise}
+        Loading...
+      {:then { data: places }}
+        {#if places}
+          {#each [...places, ...places, ...places] as place}
+            <li>
+              <a href={`/places/${place.place_id}`}>
+                {@html generateStackOfIcons({
+                  threeAvatars: getThreeAvatarUrls(place) ?? [],
+                  indicator: (place.people ?? []).length
+                })}
 
-              {place.description}
-            </a>
-          </li>
-        {/each}
-      {/if}
-    {:catch name}
-      {name}
-    {/await}
-  </ul>
+                {place.description}
+              </a>
+            </li>
+          {/each}
+        {/if}
+      {:catch name}
+        {name}
+      {/await}
+    </ul>
+  </div>
+
   <div class="form-control">
-    <div class="divider" />
     <a class="btn btn-secondary btn-block space-x-2" href="/map">
       <!-- Insert an svg of a map -->
       <svg
