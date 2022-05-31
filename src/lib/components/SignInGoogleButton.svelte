6266641a82d5a74d46ecfe2605cfbaaa3139de02
Braden Wong <bmw02002turbo@gmail.com>
2022-05-30 19:13:48 -0700

    Refactor: Google Icon

--- src/lib/components/SignInGoogleButton.svelte
+++ src/lib/components/SignInGoogleButton.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
+  import GoogleIcon from './icons/GoogleIcon.svelte';
+
   import { signIn, authLoadingStore } from '$lib/stores/auth/authLoadingStore';
 </script>
 
 <button class:loading={$authLoadingStore} class="btn" on:click={signIn}>
   <div class="left">
-    <img
-      width="20px"
-      style="margin-top:7px; margin-right:8px"
-      alt="Google sign-in"
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-      referrerpolicy="no-referrer"
-    />
+    <GoogleIcon />
   </div>
   Login with Google
 </button>
