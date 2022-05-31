1e56c17f29b033427c7e3e4e8d1803b7b365ca49
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 09:49:27 -0700

    Chore: Imports

--- src/lib/components/TableOfLocations.svelte
+++ src/lib/components/TableOfLocations.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { definitionsJSON, Person } from '$lib/types/definitionsJSON';
-  import { generateStackOfIcons } from '$lib/utils/map/generateInnerHTML';
 
   import { supabase } from '$lib/utils/supabaseClient';
 
@@ -11,41 +10,8 @@
   function getPeopleNames(place: definitionsJSON['places_with_people']) {
     return place.people?.map((person) => (<Person>person).name);
   }
-
-  function getThreeAvatarUrls(place: definitionsJSON['places_with_people']) {
-    return place.people
-      ?.slice(0, 3)
-      .map((person) => (<Person>person).avatar_url);
-  }
 </script>
 
-<div class="rounded-box bg-base-100 text-base-content shadow-xl">
-  <ul class="menu overflow-visible p-3">
-    <li class="menu-title">
-      <span>Places</span>
-    </li>
-    {#await promise}
-      Loading...
-    {:then { data: places }}
-      {#if places}
-        {#each places as place}
-          <li>
-            <a href={`/places/${place.place_id}`}>
-              {@html generateStackOfIcons({
-                threeAvatars: getThreeAvatarUrls(place) ?? [],
-                indicator: (place.people ?? []).length
-              })}
-
-              {place.description}
-            </a>
-          </li>
-        {/each}
-      {/if}
-    {:catch name}
-      {name}
-    {/await}
-  </ul>
-</div>
 <table class="table w-full">
   <!-- head -->
   <thead>
