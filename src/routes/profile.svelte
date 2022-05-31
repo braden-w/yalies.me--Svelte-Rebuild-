6cb9c5fca5b624043e9151d335b388c467df80dd
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 10:34:30 -0700

    Feat: Shrink User Cards

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -21,14 +21,14 @@
       class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 md:grid md:grid-flow-col md:grid-cols-12 md:grid-rows-6"
     >
       <!-- Cell 1 -->
-      <div class="col-span-4 row-span-3">
+      <div class="col-span-4 row-span-3 xl:col-span-2">
         <UserCard bind:userProfileInformation={$profileStore} />
         <div class="rounded-box mt-4 bg-neutral py-2 shadow-xl xl:w-full">
           <UserSocials bind:userProfileInformation={$profileStore} />
         </div>
       </div>
       <!-- Cell 2 -->
-      <div class="col-span-4 row-span-3">
+      <div class="col-span-4 row-span-3 xl:col-span-2">
         <SpotifyPlayer url={$profileStore?.expression} />
         <!-- <UserAvatarsRow {userProfileInformation} /> -->
       </div>
@@ -41,7 +41,7 @@
         <ListOfLocations />
       </div>
       <!-- Cell 5 -->
-      <div class="col-span-4 row-span-6 xl:col-span-6">
+      <div class="col-span-4 row-span-6 xl:col-span-8">
         <UserResponsesCard bind:userProfileInformation={$profileStore} />
       </div>
       <!-- <div class="col-span-2 row-span-3"> -->
