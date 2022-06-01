fd268b332c30fecf72c6ed6916106da848a9b4d9
Braden Wong <bmw02002turbo@gmail.com>
2022-06-01 06:43:44 -0700

    Feat: PlacesAndTheirStore

--- src/lib/stores/placesAndTheirPeopleStore.ts
+++ src/lib/stores/placesAndTheirPeopleStore.ts
@@ -1,16 +1,18 @@
 import type { definitionsJSON } from '$lib/types/definitionsJSON';
 import createStore from '$lib/utils/createStore';
 import { supabase } from '$lib/utils/supabaseClient';
-import type {Writable} from 'svelte/store';
-
+import type { Writable } from 'svelte/store';
 
 export async function refreshPlacesAndTheirPeopleStore() {
-      const { data: places, error } = await supabase
-      .from<definitionsJSON['places_with_people']>('places_with_people')
-      .select('*')
-      .not('people', 'is', null);
+  const { data: places, error } = await supabase
+    .from<definitionsJSON['places_with_people']>('places_with_people')
+    .select('*')
+    .not('people', 'is', null);
   if (places) {
-    console.log("🚀 ~ file: placesAndTheirPeopleStore.ts ~ line 14 ~ refreshProfileStore ~ places", places)
+    console.log(
+      '🚀 ~ file: placesAndTheirPeopleStore.ts ~ line 14 ~ refreshProfileStore ~ places',
+      places
+    );
     placesAndTheirPeopleStore.set(places);
     return places;
   }
@@ -21,12 +23,12 @@ export async function refreshPlacesAndTheirPeopleStore() {
   return null;
 }
 
-/** A store that lists places and their people. 
+/** A store that lists places and their people.
  * Lists all places with people, and their corresponding people.
-*/
+ */
 export const placesAndTheirPeopleStore: Writable<
-      definitionsJSON['places_with_people'] | null
-> = createStore<definitionsJSON['places_with_people'] | null>(
+  definitionsJSON['places_with_people'][] | null
+> = createStore<definitionsJSON['places_with_people'][] | null>(
   'yalies.me-placesAndTheirPeopleStore',
   null
 );
