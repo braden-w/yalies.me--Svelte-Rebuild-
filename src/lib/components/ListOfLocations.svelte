b4386114ac64bef6f02a10b3e6773e65f37bc808
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 19:03:45 -0700

    Feat: Locations List

--- src/lib/components/ListOfLocations.svelte
+++ src/lib/components/ListOfLocations.svelte
@@ -19,45 +19,41 @@
 </script>
 
 <div
-  class="rounded-box mx-2 flex flex-shrink-0 flex-col justify-center gap-4 bg-base-100 p-4 shadow-xl xl:mx-0 xl:w-full"
+  class="rounded-box max-h-96 overflow-y-auto bg-neutral text-base-content shadow-xl"
 >
-  <div
-    class="rounded-box max-h-96 overflow-y-auto bg-neutral text-base-content shadow-xl"
-  >
-    <ul class="menu overflow-visible p-3">
-      <li class="menu-title">
-        <span>Locations</span>
-      </li>
-      {#await promise}
-        Loading...
-      {:then { data: places }}
-        {#if places}
-          {#each places as place}
-            <li>
-              <a href={`/places/${place.place_id}`}>
-                {@html generateStackOfIcons({
-                  threeAvatars: getThreeAvatarUrls(place) ?? [],
-                  indicator: (place.people ?? []).length
-                })}
+  <ul class="menu overflow-visible p-3">
+    <li class="menu-title">
+      <span>Locations</span>
+    </li>
+    {#await promise}
+      Loading...
+    {:then { data: places }}
+      {#if places}
+        {#each places as place}
+          <li>
+            <a href={`/places/${place.place_id}`}>
+              {@html generateStackOfIcons({
+                threeAvatars: getThreeAvatarUrls(place) ?? [],
+                indicator: (place.people ?? []).length
+              })}
 
-                {place.description}
-              </a>
-            </li>
-          {/each}
-        {/if}
-      {:catch name}
-        {name}
-      {/await}
-    </ul>
-  </div>
+              {place.description}
+            </a>
+          </li>
+        {/each}
+      {/if}
+    {:catch name}
+      {name}
+    {/await}
+  </ul>
+</div>
 
-  <div class="form-control">
-    <!-- <div class="divider" /> -->
-    <a class="btn btn-secondary btn-block space-x-2" href="/locations">
-      <!-- Insert an svg of a list of Locations -->
-      <LocationsListIcon />
+<div class="form-control">
+  <!-- <div class="divider" /> -->
+  <a class="btn btn-secondary btn-block space-x-2" href="/locations">
+    <!-- Insert an svg of a list of Locations -->
+    <LocationsListIcon />
 
-      <span>Go to Locations</span>
-    </a>
-  </div>
+    <span>Go to Locations</span>
+  </a>
 </div>
