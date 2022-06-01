31af59d1ed7cfff6e6aa865b5a97aee953b3fc17
Braden Wong <bmw02002turbo@gmail.com>
2022-06-01 07:23:32 -0700

    Feat: Get Started

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,66 +39,36 @@
       <!-- <p class="mx-auto mb-5 w-full max-w-lg">Caption</p> -->
       <!-- <div class="font-bold">Creating a button</div> -->
       <!-- using only utility classes vs using daisyUI component classes -->
-      <div class="mt-10 mb-20 flex flex-col lg:flex-row">
-        <div class="flex w-full flex-col text-left">
-          <div class="mt-4 flex h-16 items-start justify-center">
-            <button
-              class="inline-block cursor-pointer rounded-md bg-indigo-600 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-indigo-700"
-            >
-              New Haven
-            </button>
-          </div>
-        </div>
-        <div
-          class="divider uppercase text-base-content/50 lg:divider-horizontal"
+      <div class="my-2 flex flex-row justify-between">
+        <a
+          href="/map"
+          sveltekit:prefetch
+          class="btn btn-ghost btn-md shrink grow basis-0"
         >
-          vs
-        </div>
-        <div class="flex w-full flex-col text-left">
-          <div class="mt-4 flex h-16 items-start justify-center">
-            <button
-              class="inline-block cursor-pointer rounded-md bg-indigo-600 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-indigo-700"
-              >Map</button
-            >
-          </div>
-        </div>
-        <div
-          class="divider uppercase text-base-content/50 lg:divider-horizontal"
+          <MapIcon /> People in New Haven
+        </a>
+        <div class="divider divider-horizontal" />
+        <a
+          href="/map"
+          sveltekit:prefetch
+          class="btn btn-ghost btn-md shrink grow basis-0"
         >
-          vs
-        </div>
-        <div class="flex w-full flex-col text-left">
-          <div class="mx-auto w-full max-w-xs flex-grow shadow-lg sm:max-w-md">
-            <ListOfLocations places={$placesAndTheirPeopleStore} />
-          </div>
-          <div class="mt-4 flex h-16 items-start justify-center">
-            <button class="btn btn-primary">Button</button>
-          </div>
-        </div>
+          <MapIcon /> Go To Map
+        </a>
+        <div class="divider divider-horizontal" />
+        <a
+          href="/locations"
+          sveltekit:prefetch
+          class="btn btn-ghost btn-md shrink grow basis-0"
+        >
+          <LocationsListIcon /> See All Locations
+        </a>
       </div>
-
       <div class="mt-6 mb-10 flex w-full justify-center">
-        <a href="/components" class="btn btn-primary btn-wide"
-          >See All Components</a
-        >
+        <a href="/components" class="btn btn-primary btn-wide">
+          See All Components
+        </a>
       </div>
     </div>
   </div>
-</div>
-<div class="my-2 flex flex-row justify-between">
-  <a
-    href="/map"
-    sveltekit:prefetch
-    class="btn btn-ghost btn-md shrink grow basis-0"
-  >
-    <MapIcon /> Go To Map
-  </a>
-  <div class="divider divider-horizontal" />
-  <a
-    href="/locations"
-    sveltekit:prefetch
-    class="btn btn-ghost btn-md shrink grow basis-0"
-  >
-    <LocationsListIcon /> See All Locations
-  </a>
 </div>
