ef5f56cf06ef0303f077e627aa0d25fe2df19f71
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 18:30:09 -0700

    Feat: Index which is the same as landing

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import LoginSplashScreen from '../lib/components/LoginSplashScreen.svelte';
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Landing</title>
+  <meta name="description" content="Log into the map!" />
+</svelte:head>
+
+<LoginSplashScreen />
