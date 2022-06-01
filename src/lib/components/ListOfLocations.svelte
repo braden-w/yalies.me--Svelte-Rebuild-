457a9c40dd9e95c1cdb84185a3eb623fbc864185
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 19:13:55 -0700

    Feat: Move to Prerendering of Parent Route

--- src/lib/components/ListOfLocations.svelte
+++ src/lib/components/ListOfLocations.svelte
@@ -4,18 +4,12 @@
   import type { definitionsJSON, Person } from '$lib/types/definitionsJSON';
   import { generateStackOfIcons } from '$lib/utils/map/generateInnerHTML';
 
-  import { supabase } from '$lib/utils/supabaseClient';
-
-  let promise = supabase
-    .from<definitionsJSON['places_with_people']>('places_with_people')
-    .select('*')
-    .not('people', 'is', null);
-
   function getThreeAvatarUrls(place: definitionsJSON['places_with_people']) {
     return place.people
       ?.slice(0, 3)
       .map((person) => (<Person>person).avatar_url);
   }
+  export let places: definitionsJSON['users_facebook_places'][] | null;
 </script>
 
 <div
@@ -25,26 +19,20 @@
     <li class="menu-title">
       <span>Locations</span>
     </li>
-    {#await promise}
-      Loading...
-    {:then { data: places }}
-      {#if places}
-        {#each places as place}
-          <li>
-            <a href={`/places/${place.place_id}`}>
-              {@html generateStackOfIcons({
-                threeAvatars: getThreeAvatarUrls(place) ?? [],
-                indicator: (place.people ?? []).length
-              })}
+    {#if places}
+      {#each places as place}
+        <li>
+          <a href={`/places/${place.place_id}`}>
+            {@html generateStackOfIcons({
+              threeAvatars: getThreeAvatarUrls(place) ?? [],
+              indicator: (place.people ?? []).length
+            })}
 
-              {place.description}
-            </a>
-          </li>
-        {/each}
-      {/if}
-    {:catch name}
-      {name}
-    {/await}
+            {place.description}
+          </a>
+        </li>
+      {/each}
+    {/if}
   </ul>
 </div>
 
