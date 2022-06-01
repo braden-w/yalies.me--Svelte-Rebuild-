b7b849095b42efc46e530fa531aa4e3f45c59ac9
Braden Wong <bmw02002turbo@gmail.com>
2022-05-31 18:59:03 -0700

    Feat: Padding

--- src/routes/locations.svelte
+++ src/routes/locations.svelte
@@ -18,7 +18,10 @@
 </script>
 
 <script lang="ts">
+  import { profileStore } from '$lib/stores/auth/profileStore';
+  import LocationAutoComplete from '$lib/components/LocationAutoComplete.svelte';
   import TableOfLocations from '$lib/components/TableOfLocations.svelte';
+  import MapIcon from '$lib/components/icons/MapIcon.svelte';
 
   export let places: definitionsJSON['places_with_people'][] = [];
 </script>
@@ -51,7 +54,49 @@
 	</div>
 </div> -->
 <!-- <div class="hero min-h-screen-nav from-primary via-secondary to-base-200 bg-gradient-to-b text-primary-content grid" > -->
-<div class="hero min-h-screen bg-base-100">
+<div class="hero grid bg-base-200 p-4 text-base-content">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Where are you now?</h1>
+      <p class="py-6">
+        Enter your current city. For privacy, feel free to use a city that is in
+        proximity rather than exact location.
+      </p>
+    </div>
+    <div class="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+      <div class="card-body text-base-content">
+        <div class="text-center">
+          <div class="avatar mx-auto">
+            <div class="w-28 rounded">
+              <img
+                src={$profileStore?.avatar_url}
+                alt="Profile"
+                width="100%"
+                height="100%"
+                referrerpolicy="no-referrer"
+              />
+            </div>
+          </div>
+          <h1 class="text-2xl font-bold">{$profileStore?.name}</h1>
+          <p class="text-lg">Yale University</p>
+        </div>
+        <LocationAutoComplete
+          query={$profileStore?.description}
+          isCurrentUser={true}
+        />
+
+        <div class="form-control mt-6">
+          <a href="/map" class="btn btn-primary" sveltekit:prefetch>
+            <!-- <MapIcon /> -->
+            Go To Map
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="hero bg-base-100 p-4">
   <div class="hero-content w-full flex-row flex-wrap">
     <div class="w-full overflow-x-auto">
       <div class="w-full">
