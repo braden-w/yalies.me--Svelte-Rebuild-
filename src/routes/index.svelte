9800219bf95f1bc59dd3d6640d0d4f023ebd16ab
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 16:09:53 -0700

    Feat: Modal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,9 @@
 </script>
 
 <script lang="ts">
+  import LocationAutoComplete from '$lib/components/LocationAutoComplete.svelte';
   import LocationsPage from '$lib/components/pages/LocationsPage.svelte';
+  import { profileStore } from '$lib/stores/auth/profileStore';
   import { refreshPlacesAndTheirPeopleStore } from '$lib/stores/placesAndTheirPeopleStore';
 
   export const prerender = true;
@@ -23,10 +25,14 @@
 <input type="checkbox" id="my-modal-6" class="modal-toggle" />
 <div class="modal modal-open modal-bottom sm:modal-middle">
   <div class="modal-box">
-    <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="text-xl font-extrabold">Where are you now?</div>
+    <div class="my-4 text-xs text-base-content/70">
+      To continue, enter your current city. For privacy, feel free to use a city that is in proximity rather than exact
+      location.
+    </div>
+    <LocationAutoComplete isCurrentUser={true} query={$profileStore?.description ?? ''} />
     <div class="modal-action">
-      <label for="my-modal-6" class="btn">Yay!</label>
+      <label for="my-modal-6" class="btn">Okay!</label>
     </div>
   </div>
 </div>
