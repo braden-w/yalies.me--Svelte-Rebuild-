853688cbb46b29e232ed16a76345d974e0057052
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 16:45:41 -0700

    Feat: Border both parts of location input

--- src/lib/components/LocationAutoComplete.svelte
+++ src/lib/components/LocationAutoComplete.svelte
@@ -111,15 +111,36 @@
     <label class="label" for="location">
       <span class="label-text">I'm currently in...</span>
     </label>
-    <input
-      tabindex="0"
-      type="text"
-      id="location"
-      class="input input-bordered input-primary"
-      placeholder={isCurrentUser ? 'Start typing your city here...' : 'n/a'}
-      bind:value={query}
-      disabled={!isCurrentUser}
-    />
+    <div class="input-group input-bordered">
+      <input
+        tabindex="0"
+        type="text"
+        id="location"
+        class="input input-bordered w-full"
+        placeholder={isCurrentUser ? 'Start typing your city here...' : 'n/a'}
+        bind:value={query}
+        disabled={!isCurrentUser}
+      />
+      {#if isCurrentUser}
+        <button class="btn btn-ghost btn-circle border-accent border-opacity-20">
+          <!-- Insert a backspace svg -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+            />
+          </svg>
+        </button>
+      {/if}
+    </div>
   </div>
 
   <!-- For each result in results, display  -->
