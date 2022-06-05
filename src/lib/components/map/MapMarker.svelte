a21d93bf0b59b6ee869eedff078b415e3886e809
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 22:50:23 -0700

    Fix: Types

--- src/lib/components/map/MapMarker.svelte
+++ src/lib/components/map/MapMarker.svelte
@@ -7,27 +7,10 @@
   const { getMap } = getContext(key);
   const map = getMap();
 
-  export let place: definitionsJSON['places_with_people'];
+  export let place: definitionsJSON['places_with_people'] | definitionsJSON['places_with_facebook'];
 
   let el: HTMLDivElement;
   onMount(() => {
-    const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
-      const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
-      return defaultPxSize * scalePercent;
-    };
-
-    /** Scale icons on zoom */
-    map.on('zoom', () => {
-      const newPx = scalePercent();
-      console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
-      el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
-        // Set the height and width innerEl to newPx
-        innerEl.style.width = `${newPx}px`;
-        innerEl.style.height = `${newPx}px`;
-      });
-      el.style.transformOrigin = 'bottom';
-    });
-
     // Add the marker to the map
     if (!place.lng || !place.lat) return;
     new mapboxgl.Marker(el).setLngLat([place.lng, place.lat]).addTo(map);
