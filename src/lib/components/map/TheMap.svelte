43d7e3c61b147e80bb78bc88be0dfcb77abd9674
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 20:41:01 -0700

    Types

--- src/lib/components/map/TheMap.svelte
+++ src/lib/components/map/TheMap.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { key } from '$lib/components/map/mapbox';
 
-  import type { definitionsJSON } from '$lib/types/definitionsJSON';
-  import { generateInnerHTML } from '$lib/utils/map/generateInnerHTML';
   import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
   import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
   import mapboxgl from 'mapbox-gl';
@@ -11,7 +9,6 @@
 
   // const NewHaven = { longitude: -72.9, latitude: 41.3, zoom: 8 };
   const CenterUS = { longitude: -95.7, latitude: 37.1, zoom: 2 };
-  export let zoom;
 
   let generateFacebookMarkers: () => void;
 
@@ -19,8 +16,8 @@
     getMap: () => map,
   });
 
-  let container;
-  let map;
+  let container: HTMLDivElement;
+  let map: mapboxgl.Map;
   onDestroy(() => {
     if (map) map.remove();
   });
@@ -57,54 +54,54 @@
 
   // generateMarkers(map, $facebook);
   /** Add a marker to the map for each place */
-  function generateMarkers(
-    map: mapboxgl.Map,
-    places: definitionsJSON['places_with_people'][] | definitionsJSON['places_with_facebook'][] | null
-  ) {
-    if (!places) return;
-    places.forEach((place) => {
-      const el = document.createElement('div');
-      el.className = 'marker';
-      el.innerHTML = generateInnerHTML(place);
+  // function generateMarkers(
+  //   map: mapboxgl.Map,
+  //   places: definitionsJSON['places_with_people'][] | definitionsJSON['places_with_facebook'][] | null
+  // ) {
+  //   if (!places) return;
+  //   places.forEach((place) => {
+  //     const el = document.createElement('div');
+  //     el.className = 'marker';
+  //     el.innerHTML = generateInnerHTML(place);
 
-      // On click, add a shadow around it
-      el.addEventListener('click', () => {
-        // For every element in el with a class "outline-on-click", add the class "ring" to it
-        el.querySelectorAll('.outline-on-click').forEach((el) => {
-          el.classList.add('ring');
-        });
-      });
+  //     // On click, add a shadow around it
+  //     el.addEventListener('click', () => {
+  //       // For every element in el with a class "outline-on-click", add the class "ring" to it
+  //       el.querySelectorAll('.outline-on-click').forEach((el) => {
+  //         el.classList.add('ring');
+  //       });
+  //     });
 
-      // On click out, add a shadow around it
-      document.getElementsByClassName('mapboxgl-canvas')[0].addEventListener('click', () => {
-        // For every element in el with a class "outline-on-click", remove the class "ring" to it if it exists
-        el.querySelectorAll('.outline-on-click').forEach((el) => {
-          el.classList.remove('ring');
-        });
-      });
+  //     // On click out, add a shadow around it
+  //     document.getElementsByClassName('mapboxgl-canvas')[0].addEventListener('click', () => {
+  //       // For every element in el with a class "outline-on-click", remove the class "ring" to it if it exists
+  //       el.querySelectorAll('.outline-on-click').forEach((el) => {
+  //         el.classList.remove('ring');
+  //       });
+  //     });
 
-      const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
-        const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
-        return defaultPxSize * scalePercent;
-      };
+  //     const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
+  //       const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
+  //       return defaultPxSize * scalePercent;
+  //     };
 
-      /** Scale icons on zoom */
-      map.on('zoom', () => {
-        const newPx = scalePercent();
-        console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
-        el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
-          // Set the height and width innerEl to newPx
-          innerEl.style.width = `${newPx}px`;
-          innerEl.style.height = `${newPx}px`;
-        });
-        el.style.transformOrigin = 'bottom';
-      });
+  //     /** Scale icons on zoom */
+  //     map.on('zoom', () => {
+  //       const newPx = scalePercent();
+  //       console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
+  //       el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
+  //         // Set the height and width innerEl to newPx
+  //         innerEl.style.width = `${newPx}px`;
+  //         innerEl.style.height = `${newPx}px`;
+  //       });
+  //       el.style.transformOrigin = 'bottom';
+  //     });
 
-      // Add the marker to the map
-      if (!place.lng || !place.lat) return;
-      new mapboxgl.Marker(el).setLngLat([place.lng, place.lat]).addTo(map);
-    });
-  }
+  //     // Add the marker to the map
+  //     if (!place.lng || !place.lat) return;
+  //     new mapboxgl.Marker(el).setLngLat([place.lng, place.lat]).addTo(map);
+  //   });
+  // }
 </script>
 
 <svelte:head>
