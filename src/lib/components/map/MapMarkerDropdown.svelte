141c9e2e2c6bfa046a91efb0c63a9b91e8c1dda2
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 21:17:21 -0700

    Feat: No more .image

---
+++ src/lib/components/map/MapMarkerDropdown.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { definitionsJSON, Person, PersonFromFacebook } from '$lib/types/definitionsJSON';
+  import { generateHover, generateStackOfIcons } from '$lib/utils/map/generateInnerHTML';
+
+  export let place: definitionsJSON['places_with_people'] | definitionsJSON['places_with_facebook'];
+
+  // Get 3 random people from the 'people' property of placeWithPeople
+  const people = place.people ?? [];
+  /** The first three people who will be the icons in the stack on the map */
+  const stackIcons = people
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3)
+    .map((person) => {
+      return person.avatar_url;
+    });
+  const { place_id, description } = place;
+
+  // Three cases for the number of people in the placeWithPeople
+  // if (stackIcons.length === 0) return '';
+</script>
+
+<div class="dropdown dropdown-hover">
+  {@html generateStackOfIcons({
+    threeAvatars: stackIcons,
+    indicator: people.length,
+  })}
+  {@html generateHover({
+    numberOfIconsStacked: stackIcons.length,
+    place_id: place_id ?? '',
+    description: description ?? '',
+    people,
+  })}
+</div>
