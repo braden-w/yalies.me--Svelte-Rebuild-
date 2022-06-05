f2b06db5b4ebf7253dcea1ec38fc8bf5fe711697
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 21:41:23 -0700

    Chore: Remove comments

--- src/lib/components/map/TheMap.svelte
+++ src/lib/components/map/TheMap.svelte
@@ -50,57 +50,6 @@
     });
     // generateMarkers(map, $placesAndTheirPeopleStore);
   }
-
-  // generateMarkers(map, $facebook);
-  /** Add a marker to the map for each place */
-  // function generateMarkers(
-  //   map: mapboxgl.Map,
-  //   places: definitionsJSON['places_with_people'][] | definitionsJSON['places_with_facebook'][] | null
-  // ) {
-  //   if (!places) return;
-  //   places.forEach((place) => {
-  //     const el = document.createElement('div');
-  //     el.className = 'marker';
-  //     el.innerHTML = generateInnerHTML(place);
-
-  //     // On click, add a shadow around it
-  //     el.addEventListener('click', () => {
-  //       // For every element in el with a class "outline-on-click", add the class "ring" to it
-  //       el.querySelectorAll('.outline-on-click').forEach((el) => {
-  //         el.classList.add('ring');
-  //       });
-  //     });
-
-  //     // On click out, add a shadow around it
-  //     document.getElementsByClassName('mapboxgl-canvas')[0].addEventListener('click', () => {
-  //       // For every element in el with a class "outline-on-click", remove the class "ring" to it if it exists
-  //       el.querySelectorAll('.outline-on-click').forEach((el) => {
-  //         el.classList.remove('ring');
-  //       });
-  //     });
-
-  //     const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
-  //       const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
-  //       return defaultPxSize * scalePercent;
-  //     };
-
-  //     /** Scale icons on zoom */
-  //     map.on('zoom', () => {
-  //       const newPx = scalePercent();
-  //       console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
-  //       el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
-  //         // Set the height and width innerEl to newPx
-  //         innerEl.style.width = `${newPx}px`;
-  //         innerEl.style.height = `${newPx}px`;
-  //       });
-  //       el.style.transformOrigin = 'bottom';
-  //     });
-
-  //     // Add the marker to the map
-  //     if (!place.lng || !place.lat) return;
-  //     new mapboxgl.Marker(el).setLngLat([place.lng, place.lat]).addTo(map);
-  //   });
-  // }
 </script>
 
 <svelte:head>
