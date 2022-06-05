f8a5ef49d67e10f3c7c8ad533a2b6f705cfed246
Braden Wong <bmw02002turbo@gmail.com>
2022-06-04 21:42:45 -0700

    Refactor: Into Onmount

--- src/lib/components/map/MapMarker.svelte
+++ src/lib/components/map/MapMarker.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import MapDropdown from './MapMarkerDropdown.svelte';
+  import MapMarkerDropdown from '$lib/components/map/MapMarkerDropdown.svelte';
 
   import type { definitionsJSON } from '$lib/types/definitionsJSON.js';
   import { getContext, onMount } from 'svelte';
@@ -28,29 +28,29 @@
       });
     });
 
+    const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
+      const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
+      return defaultPxSize * scalePercent;
+    };
+
+    /** Scale icons on zoom */
+    map.on('zoom', () => {
+      const newPx = scalePercent();
+      console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
+      el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
+        // Set the height and width innerEl to newPx
+        innerEl.style.width = `${newPx}px`;
+        innerEl.style.height = `${newPx}px`;
+      });
+      el.style.transformOrigin = 'bottom';
+    });
+
     // Add the marker to the map
     if (!place.lng || !place.lat) return;
     new mapboxgl.Marker(el).setLngLat([place.lng, place.lat]).addTo(map);
   });
-
-  const scalePercent = (defaultPxSize = 32, defaultZoom = 2, scaleFactor = 0.1) => {
-    const scalePercent = 1 + (map.getZoom() - defaultZoom) * scaleFactor;
-    return defaultPxSize * scalePercent;
-  };
-
-  /** Scale icons on zoom */
-  map.on('zoom', () => {
-    const newPx = scalePercent();
-    console.log('🚀 ~ file: map.svelte ~ line 135 ~ map.on ~ newPx', newPx);
-    el.querySelectorAll<HTMLElement>('.outline-on-click').forEach((innerEl) => {
-      // Set the height and width innerEl to newPx
-      innerEl.style.width = `${newPx}px`;
-      innerEl.style.height = `${newPx}px`;
-    });
-    el.style.transformOrigin = 'bottom';
-  });
 </script>
 
 <div bind:this={el} class="marker">
-  <MapDropdown {place} />
+  <MapMarkerDropdown {place} />
 </div>
