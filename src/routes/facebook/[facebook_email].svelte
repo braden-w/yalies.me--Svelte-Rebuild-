f5664134933674488a5ccb2c2abf9d39922b8741
Braden Wong <bmw02002turbo@gmail.com>
2022-06-09 19:07:54 -0700

    Feat: Facebook Person Page

--- src/routes/facebook/[facebook_email].svelte
+++ src/routes/facebook/[facebook_email].svelte
@@ -29,20 +29,12 @@
 
 <div class="min-h-screen-nav hero bg-base-200">
   <div class="h-full w-full">
-    <div
-      id="component-demo"
-      class="flex w-full flex-col items-stretch gap-4 overflow-x-auto px-4 pt-1 pb-10 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6"
-    >
       <!-- Cell 1 -->
       <div class="col-span-4 row-span-3 xl:col-span-2">
         <UserCard {userProfileInformation} />
       </div>
-
       <!-- Cell 2 -->
-      <div class="col-span-2 row-span-3">
-        <UserLocationCard {userProfileInformation} />
-      </div>
-    </div>
+        <!-- <UserLocationCard {userProfileInformation} /> -->
   </div>
 </div>
 <!-- <div class="divider divider-horizontal">OR</div> -->
